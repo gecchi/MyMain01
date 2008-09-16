ffc6d757fb1c37a45730ef764dc8524cb6104449
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2008-09-16 18:37:25 +0000

    git-svn-id: file:///home/gecchi/repos@81 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/RotationActor.cpp
+++ MyStg2nd/src/gecchi/actor/RotationActor.cpp
@@ -14,6 +14,7 @@ GgafActor* RotationActor::getFreeOne() {
 		if(pActor->isPlaying()) {
 			if (pActor->isLast()) {
 				pActor = NULL;
+				break;
 			} else {
 				pActor = pActor->getNext();
 			}
