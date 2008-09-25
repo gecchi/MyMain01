2131905cddde49736de3e1612cb687c849e86f90
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2008-09-25 15:57:04 +0000

    git-svn-id: file:///home/gecchi/repos@132 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafCore/include/jp/ggaf/core/GgafOrder.h
+++ GgafCore/include/jp/ggaf/core/GgafOrder.h
@@ -1,11 +1,19 @@
 #ifndef GGAFORDER_H_
 #define GGAFORDER_H_
 
+/**
+ * 商品（何かのインスタンス）の注文クラス .
+ * 工場(GgafFactory)に渡す注文を表したクラスで、 商品（何かのインスタンス）１つを内部保持します。<BR>
+ * また注文オブジェクト同士で連結リストを作成でき、そのリストは工場(GgafFactory)が操作することになります。<BR>
+ * @version 1.00
+ * @since 2008/06/20
+ * @auther Masatoshi Tsuge
+ */
 class GgafOrder : public GgafObject {
 	friend class GgafGod;
 	friend class GgafFactory;
-	
-private:	
+
+private:
 	/** 注文識別ID */
 	string _id;
 	/** 一つ次の注文 */
@@ -24,7 +32,7 @@ private:
 	void* _argumentForBuild;
 	/** 進捗具合（0:未着手/1:製造中/2:製造済み） */
 	int _progress;
-	
+
 public:
 	GgafOrder(string prm_id);
 	virtual ~GgafOrder();
