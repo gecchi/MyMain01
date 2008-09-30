0c5635ac80094a174d023ed18c4460c316826ab7
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2008-09-30 17:00:57 +0000

    git-svn-id: file:///home/gecchi/repos@177 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- CustomizeVorbis_static/lib/window.c
+++ CustomizeVorbis_static/lib/window.c
@@ -1,3 +1,4 @@
+
 /********************************************************************
  *                                                                  *
  * THIS FILE IS PART OF THE OggVorbis SOFTWARE CODEC SOURCE CODE.   *
