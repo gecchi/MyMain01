ce48f0be1eedab1015df4f3557ab7b7271df935e
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2008-10-02 13:49:52 +0000

    git-svn-id: file:///home/gecchi/repos@188 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

---
+++ MyStg2nd/src/gecchi/actor/my/sprite/DefaultSpriteMyActor.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+
+DefaultSpriteMyActor::DefaultSpriteMyActor(string prm_name, string prm_xname) : DefaultSpriteActor(prm_name, prm_xname) {
+}
+
+DefaultSpriteMyActor::~DefaultSpriteMyActor() {
+}
---
+++ MyStg2nd/src/gecchi/actor/my/sprite/DefaultSpriteMyActor.h
@@ -0,0 +1,14 @@
+#ifndef DEFAULTSPRITEENEMYACTOR_H_
+#define DEFAULTSPRITEENEMYACTOR_H_
+
+
+class DefaultSpriteMyActor : public DefaultSpriteActor {
+
+public:
+	DefaultSpriteMyActor(string prm_name, string prm_xname);
+
+	virtual ~DefaultSpriteMyActor();
+};
+
+#endif /*DEFAULTSPRITEENEMYACTOR_H_*/
+
