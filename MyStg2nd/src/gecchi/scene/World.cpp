c661fdf70a3804f2fbf2175f44b840137147c22b
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2008-12-11 16:52:07 +0000

    git-svn-id: file:///home/gecchi/repos@452 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/scene/World.cpp
+++ MyStg2nd/src/gecchi/scene/World.cpp
@@ -11,11 +11,22 @@ World::World(string prm_name) : DefaultWorld(prm_name) {
 }
 
 void World::processDrawPrior() {
-	GgafDx9World::_pCamera->setGaze(GameGlobal::_pMyShip);
+
+//	GgafDx9World::_pCamera->setGaze(GameGlobal::_pMyShip);
+//
+//
+//
+//	GgafDx9World::_pCamera->_pGeoMover->
+//	GgafDx9World::_pCamera->Z = GameGlobal::_pMyShip->_Z - GgafDx9God::_dCamZ_ini;
+
+	if (GgafDx9World::_pCamera->_Z < GameGlobal::_pMyShip->_Z - GgafDx9God::_dCamZ_ini) {
+		GgafDx9World::_pCamera->_Z = GgafDx9World::_pCamera->_Z + 2000;
+	}
+	if (GgafDx9World::_pCamera->_Z < GameGlobal::_pMyShip->_Z - GgafDx9God::_dCamZ_ini) {
+		GgafDx9World::_pCamera->_Z = GgafDx9World::_pCamera->_Z - 2000;
+	}
+
 }
-
-
-
 
 World::~World() {
 }
