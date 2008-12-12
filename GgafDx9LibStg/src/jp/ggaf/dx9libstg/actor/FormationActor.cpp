5acced1363ab8415fc3d37de4d3e48bea1d06cc9
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2008-12-12 16:26:51 +0000

    git-svn-id: file:///home/gecchi/repos@456 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/FormationActor.cpp
+++ GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/FormationActor.cpp
@@ -2,7 +2,7 @@
 
 FormationActor::FormationActor(string prm_name) : GgafDx9UntransformedActor(prm_name, NEW GgafDx9GeometryMover(this), NEW StgChecker(this)) {
 	_class_name = "FormationsActor";
-    setBumpableOnlySelf(false);
+    setBumpableAlone(false);
 }
 
 FormationActor::~FormationActor() {
