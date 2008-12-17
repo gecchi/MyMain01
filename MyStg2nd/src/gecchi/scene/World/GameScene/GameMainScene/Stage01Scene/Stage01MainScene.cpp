d2b7c3486e9774e679030f52b1dc0602bf690593
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2008-12-17 11:35:07 +0000

    git-svn-id: file:///home/gecchi/repos@466 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/scene/World/GameScene/GameMainScene/Stage01Scene/Stage01MainScene.cpp
+++ MyStg2nd/src/gecchi/scene/World/GameScene/GameMainScene/Stage01Scene/Stage01MainScene.cpp
@@ -5,17 +5,12 @@ Stage01MainScene::Stage01MainScene(string prm_name) : DefaultScene(prm_name) {
 
 	// 以下の gen01 start ～ end はマクロにより自動生成されたコードです。
 	// gen01 start
-	DWORD dw[] = {1,100,500,1000,1400,1900,2300,2800,3700,4600,5500,6400,7300};
-	_paFrame_NextEvent = new DWORD[13];
-	for (int i = 0; i < 13; i++) {
+	DWORD dw[] = {1,101};
+	_paFrame_NextEvent = new DWORD[2];
+	for (int i = 0; i < 2; i++) {
 		_paFrame_NextEvent[i] = dw[i];
 	}
-	orderActorFactory("ID1096100", FormationCeres002, "F_Ceres002_1", "Ceres");
-	orderActorFactory("ID10961000", FormationCeres002, "F_Ceres002_2", "Ceres");
-	orderActorFactory("ID10961900", FormationCeres002, "F_Ceres002_3", "Ceres");
-	orderActorFactory("ID10962800", FormationCeres002, "F_Ceres002_4", "Ceres");
-	orderActorFactory("ID10963700", FormationCeres002, "F_Ceres002_5", "Ceres");
-	orderActorFactory("ID10964600", FormationCeres002, "F_Ceres002_6", "Ceres");
+	orderActorFactory("ID1096101", FormationCeres002, "F_Ceres002_1", "Ceres");
 	// gen01 end
 
 	stopImmediately(); //GameMainSceneが解除してくれる
@@ -31,41 +26,8 @@ void Stage01MainScene::processBehavior() {
 		switch (_dwFrame) {
 			case 1:
 				break;
-			case 100:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID1096100"));
-				break;
-			case 500:
-				orderActorFactory("ID10965500", FormationCeres002, "F_Ceres002_7", "Ceres");
-				break;
-			case 1000:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10961000"));
-				break;
-			case 1400:
-				orderActorFactory("ID10966400", FormationCeres002, "F_Ceres002_8", "Ceres");
-				break;
-			case 1900:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10961900"));
-				break;
-			case 2300:
-				orderActorFactory("ID10967300", FormationCeres002, "F_Ceres002_9", "Ceres");
-				break;
-			case 2800:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10962800"));
-				break;
-			case 3700:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10963700"));
-				break;
-			case 4600:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10964600"));
-				break;
-			case 5500:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10965500"));
-				break;
-			case 6400:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10966400"));
-				break;
-			case 7300:
-				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID10967300"));
+			case 101:
+				getLordActor()->accept(KIND_ENEMY, obtainActorFactory("ID1096101"));
 				break;
 			default :
 				break;
