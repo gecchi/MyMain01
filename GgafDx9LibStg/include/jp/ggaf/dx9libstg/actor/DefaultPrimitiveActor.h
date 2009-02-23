5d82d28b9e4bd9b76480a0f11a57081a166f2b3b
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-02-23 16:51:51 +0000

    git-svn-id: file:///home/gecchi/repos@738 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/include/jp/ggaf/dx9libstg/actor/DefaultPrimitiveActor.h
+++ GgafDx9LibStg/include/jp/ggaf/dx9libstg/actor/DefaultPrimitiveActor.h
@@ -13,7 +13,7 @@ public:
 
     StgChecker* _pChecker;
 
-    DefaultPrimitiveActor(const char* prm_name, const char* prm_model);
+    DefaultPrimitiveActor(const char* prm_name, const char* prm_model, const char* prm_effect);
 
     virtual void initialize() {
     }
