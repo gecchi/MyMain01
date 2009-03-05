0c29fbfa5b4d8e81eece56e8a04bb11a1fe8f1b2
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-03-05 16:56:25 +0000

    git-svn-id: file:///home/gecchi/repos@799 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- cppTest/src/main.cpp
+++ cppTest/src/main.cpp
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
 
 
     //std::string temp = "MYCUBE001_no.X";
-    std::string temp = "ebi2.X";
+    std::string temp = "vic2.X";
     cout << "temp=" << temp << endl;
     bool Running = false;
     CurrentModel = &MyModel;
