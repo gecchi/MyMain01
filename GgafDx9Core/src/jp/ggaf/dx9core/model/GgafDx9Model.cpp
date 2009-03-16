075cd78cdc3fad73fde08af7834d8fab1de91352
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-03-16 18:27:23 +0000

    git-svn-id: file:///home/gecchi/repos@870 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9Core/src/jp/ggaf/dx9core/model/GgafDx9Model.cpp
+++ GgafDx9Core/src/jp/ggaf/dx9core/model/GgafDx9Model.cpp
@@ -15,6 +15,7 @@ GgafDx9Model::GgafDx9Model(char* prm_model_name) : GgafObject() {
 GgafDx9Model::~GgafDx9Model() {
     TRACE3("GgafDx9Model::~GgafDx9Model() " << _model_name << " start-->");
     DELETEARR_IMPOSSIBLE_NULL(_model_name);
-    TRACE3("GgafDx9Model::~GgafDx9Model() " << _model_name << " <--end");
+	//«‚±‚ñ‚È‚±‚Æ‚µ‚Ä‚Í‚¢‚¯‚Ü‚¹‚ñ
+    //TRACE3("GgafDx9Model::~GgafDx9Model() " << _model_name << " <--end");
 }
 
