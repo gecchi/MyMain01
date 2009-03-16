531e7fcd84c4737013e7d3d439d01370e82f969f
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-03-16 17:57:47 +0000

    git-svn-id: file:///home/gecchi/repos@867 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/scene/World.cpp
+++ MyStg2nd/src/gecchi/scene/World.cpp
@@ -9,8 +9,8 @@ World::World(const char* prm_name) : DefaultWorld(prm_name) {
     GameScene* pGameScene = NEW GameScene("Game");
     addSubLast(pGameScene);
 #ifdef OREDEBUG
-//    DispFpsActor* pDispFpsActor = NEW DispFpsActor("FPS_STRING", "B/moji");
-//    getLordActor()->accept(KIND_EFFECT, pDispFpsActor);
+    DispFpsActor* pDispFpsActor = NEW DispFpsActor("FPS_STRING", "B/moji");
+    getLordActor()->accept(KIND_EFFECT, pDispFpsActor);
 #endif
 //    _pCamera->setGaze(0,0,0);
 //    _pCamera->_pGeoMover->setRyMoveAngleVelocity(1000);
