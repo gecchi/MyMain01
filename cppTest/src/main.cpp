70683dddd55b9ab10930ba2234b6367fd4136c30
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-04-07 17:18:28 +0000

    オプション整理
    git-svn-id: file:///home/gecchi/repos@954 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- cppTest/src/main.cpp
+++ cppTest/src/main.cpp
@@ -38,79 +38,16 @@ unsigned int ToDec(const char str[ ])
 
 
 int main(int argc, char *argv[]) {
+    int a, i;
+    double x;
 
-    //system("CMD /C CD C:\\cygwin\\workspace\\MyStg2nd && C:\\cygwin\\workspace\\MyStg2nd\\MyStg2nd.exe 2>&1 >> C:\\temp\\loglog.log");
-//    ShellExecute(NULL, "open", "bash.exe", "/cygdrive/c/cygwin/workspace/MyStg2nd/RunMyStg2nd.sh", NULL, SW_SHOW);
-    //ShellExecute("bash -c /cygdrive/c/cygwin/workspace/MyStg2nd/RunMyStg2nd.sh");
+    printf("a = "); scanf("%d", &a);
 
-    STARTUPINFO sinfo;
-    PROCESS_INFORMATION pinfo;
-
-    ZeroMemory( &sinfo, sizeof( STARTUPINFO ) );
-    ZeroMemory( &pinfo, sizeof( PROCESS_INFORMATION ) );
-    sinfo.cb = sizeof( STARTUPINFO );
-    sinfo.dwFlags = STARTF_USESHOWWINDOW|DEBUG_PROCESS ;
-    sinfo.wShowWindow = SW_SHOWNORMAL;
-
-//    BOOL CreateProcess(
-//      LPCTSTR lpApplicationName,                 //実行可能モジュールの名前
-//      LPTSTR lpCommandLine,                      //コマンドラインの文字列
-//      LPSECURITY_ATTRIBUTES lpProcessAttributes, //セキュリティ記述子
-//      LPSECURITY_ATTRIBUTES lpThreadAttributes,  //セキュリティ記述子
-//      BOOL bInheritHandles,                      //ハンドルの継承オプション
-//      DWORD dwCreationFlags,                     //作成のフラグ
-//      LPVOID lpEnvironment,                      //新しい環境ブロック
-//      LPCTSTR lpCurrentDirectory,                //カレントディレクトリの名前
-//      LPSTARTUPINFO lpStartupInfo,               //スタートアップ情報
-//      LPPROCESS_INFORMATION lpProcessInformation //プロセス情報
-//    );
-
-
-
-    if ( !CreateProcess( NULL,
-                         "C:\\cygwin\\workspace\\MyStg2nd\\MyStg2nd.exe",
-                         NULL,
-                         NULL,
-                         FALSE,
-                         0,
-                         NULL,
-                         "C:\\cygwin\\workspace\\MyStg2nd",
-                         &sinfo,
-                         &pinfo ) ) {
-
-        // 起動失敗
-        printf("しっぱい\n");
+    x = 1;
+    for (i = 1; i <= 10; i++) {
+        x = 0.5 * (x + a / x);
+        printf("第 %d 近似値 = %lf\n", i, x);
     }
-
-    printf("ptargetVertxBuffer=");
-
-    char name[25];
-    scanf("%s",name);
-    //パラメータ部 ------->
-    LPDIRECT3DVERTEXBUFFER9 ptargetVertxBuffer = (LPDIRECT3DVERTEXBUFFER9)ToDec(name);
-    DWORD dwFVF = (D3DFVF_XYZ | D3DFVF_NORMAL | D3DFVF_DIFFUSE | D3DFVF_TEX1);
-    int nVertxs = 10;
-    //<------------------
-
-
-
-
-
-
-    DWORD dwFVFSize  = D3DXGetFVFVertexSize(dwFVF);
-    BYTE* pByteVertexSrc;
-    D3DVECTOR* pV;
-
-    ptargetVertxBuffer->Lock(0, 0, (void**)&pByteVertexSrc, D3DLOCK_READONLY);
-
-    for (int i = 0; i < nVertxs; i++) {
-        pV = (D3DVECTOR*)(pByteVertexSrc + (dwFVFSize * i) + 0);
-        printf("頂点 %d : (%f,%f,%f)",(i+1),(pV->x),(pV->y),(pV->z));
-    }
-
-    ptargetVertxBuffer->Unlock();
-
-    return 0;
 }
 
 
