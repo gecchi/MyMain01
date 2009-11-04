a48142dc173cfa39bb2ac7f3a1fc0d4568ee7d41
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-11-04 16:52:34 +0000

    ほーみんぐできた
    git-svn-id: file:///home/gecchi/repos@1571 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/GeoElement.h
+++ GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/GeoElement.h
@@ -10,12 +10,28 @@ public:
     int _X;
     int _Y;
     int _Z;
+    int _RX;
+    int _RY;
+    int _RZ;
     GeoElement* _next;
 
-    GeoElement(int prm_X, int prm_Y, int prm_Z) {
+    GeoElement() {
+        _X = 0;
+        _Y = 0;
+        _Z = 0;
+        _RX = 0;
+        _RY = 0;
+        _RZ = 0;
+        _next = NULL;
+    }
+
+    GeoElement(int prm_X, int prm_Y, int prm_Z, int prm_RX, int prm_RY, int prm_RZ) {
         _X = prm_X;
         _Y = prm_Y;
         _Z = prm_Z;
+        _RX = prm_RX;
+        _RY = prm_RY;
+        _RZ = prm_RZ;
         _next = NULL;
     }
 
@@ -23,18 +39,27 @@ public:
         _X = prm_pActor->_X;
         _Y = prm_pActor->_Y;
         _Z = prm_pActor->_Z;
+        _RX = prm_pActor->_RX;
+        _RY = prm_pActor->_RY;
+        _RZ = prm_pActor->_RZ;
         _next = NULL;
     }
 
-    void set(int prm_X, int prm_Y, int prm_Z) {
+    void set(int prm_X, int prm_Y, int prm_Z, int prm_RX, int prm_RY, int prm_RZ) {
         _X = prm_X;
         _Y = prm_Y;
         _Z = prm_Z;
+        _RX = prm_RX;
+        _RY = prm_RY;
+        _RZ = prm_RZ;
     }
     void set(GgafDx9Core::GgafDx9GeometricActor* prm_pActor) {
         _X = prm_pActor->_X;
         _Y = prm_pActor->_Y;
         _Z = prm_pActor->_Z;
+        _RX = prm_pActor->_RX;
+        _RY = prm_pActor->_RY;
+        _RZ = prm_pActor->_RZ;
     }
     virtual ~GeoElement() {
         _next = NULL;
