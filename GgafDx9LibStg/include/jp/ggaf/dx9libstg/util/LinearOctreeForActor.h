4843b5be344500ce0ab5ac93ed4bd249de47409b
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-11-12 16:54:46 +0000

    git-svn-id: file:///home/gecchi/repos@1615 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/LinearOctreeForActor.h
+++ GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/LinearOctreeForActor.h
@@ -8,42 +8,104 @@ public:
 
     class ElemEx : public GgafLinearOctree::Elem {
     public:
-
+        GgafCore::GgafActor* _pActor;
         ElemEx(GgafCore::GgafObject* prm_pObject, DWORD prm_kindbit) : GgafLinearOctree::Elem(prm_pObject,prm_kindbit) {
-            //_pActorList = (ActorList*)prm_pObject;
-
+            _pActor = (GgafCore::GgafActor*)prm_pObject;
         }
     };
 
-    class ActorList;
 
-    class KindMap {
+    class CollisionList {
     public:
-        class Node {
+        class Elem {
         public:
-            actorkind  key;
-            ActorList* _pActor;
+            /** ŽŸ—v‘f */
+            Elem* _pNext;
+            /** ‘O—v‘f */
+            Elem* _pPrev;
+            /** ’l */
+            GgafCore::GgafActor* _pValue;
+
+            Elem(GgafCore::GgafActor* prm_pActor) {
+                _pValue = prm_pActor;
+                _pNext = NULL;
+                _pPrev = NULL;
+            }
+
+            ~Elem() {}
         };
 
-        Node _node[10];
-        KindMap() {
+        Elem* _pFirst;
+        Elem* _pLast;
+        CollisionList() {
+            _pFirst = NULL;
+            _pLast = NULL;
         }
-        ActorList* getActorList(actorkind prm_key) {
-            for (int i = 10; i < 10; i++) {
-                if (prm_key == _node[i].key) {
-                    return _node[i]._pActor;
-                }
+
+        void push(GgafCore::GgafActor* prm_pActor) {
+            Elem* pNew = NEW Elem(prm_pActor);
+            if (_pFirst) {
+                _pLast->_pNext = pNew;
+                pNew->_pPrev = _pLast;
+            } else {
+                _pFirst = pNew;
+                _pLast = _pFirst;
             }
-            return NULL;
+        }
+        GgafCore::GgafActor* pop() {
+            if (_pFirst) {
+                Elem* pRet = _pLast;
+                GgafCore::GgafActor* pVal = pRet->_pValue; //”O‚Ì‚½‚ß
+                _pLast->_pPrev->_pNext = NULL;
+                _pLast = _pLast ->_pPrev;
+                delete pRet;
+                return pVal;
+            } else {
+                return NULL;
+            }
         }
 
-      
-
+        ~CollisionList() {}
     };
 
-    void executeBumpChk(int index);
 
-    void executeAllBumpChk(actorkind prm_paaActorkindMaskPair[][2], int prm_mnum);
+    CollisionList _listTreeGroupA;
+    CollisionList _listTreeGroupB;
+
+    CollisionList _listGroupA;
+    CollisionList _listGroupB;
+
+    actorkind _kind_groupA;
+    actorkind _kind_groupB;
+
+    void executeBumpChk_RoundRobin(CollisionList* prm_pListA, CollisionList* prm_pListB);
+//    class KindMap {
+//    public:
+//        class Node {
+//        public:
+//            actorkind  key;
+//            ActorList* _pActor;
+//        };
+//
+//        Node _node[10];
+//        KindMap() {
+//        }
+//        ActorList* getActorList(actorkind prm_key) {
+//            for (int i = 10; i < 10; i++) {
+//                if (prm_key == _node[i].key) {
+//                    return _node[i]._pActor;
+//                }
+//            }
+//            return NULL;
+//        }
+//
+//
+//
+//    };
+
+    void executeBumpChk(int prm_index);
+
+    void executeAllBumpChk(actorkind prm_groupA, actorkind prm_groupB);
 
 
     LinearOctreeForActor(int prm_level);
--- GgafDx9LibStg/src/jp/ggaf/dx9libstg/util/LinearOctreeForActor.cpp
+++ GgafDx9LibStg/src/jp/ggaf/dx9libstg/util/LinearOctreeForActor.cpp
@@ -9,37 +9,84 @@ LinearOctreeForActor::LinearOctreeForActor(int prm_level) : GgafLinearOctree(prm
 }
 
 
-void LinearOctreeForActor::executeAllBumpChk(actorkind prm_paaActorkindMaskPair[][2], int prm_pair_num) {
-    for (int i = 0; i < prm_pair_num; i++) {
-        //actorkind group_A = prm_actorkindmask_pair[i][0];
-       // actorkind group_B = prm_actorkindmask_pair[i][1];
-    }
+void LinearOctreeForActor::executeAllBumpChk(actorkind prm_groupA, actorkind prm_groupB) {
+//    for (int i = 0; i < prm_pair_num; i++) {
+//        //actorkind group_A = prm_actorkindmask_pair[i][0];
+//       // actorkind group_B = prm_actorkindmask_pair[i][1];
+//    }
+
+    _kind_groupA = prm_groupA;
+    _kind_groupB = prm_groupB;
     executeBumpChk(0);
 }
 
 void LinearOctreeForActor::executeBumpChk(int prm_index) {
     Space* pSpace = &(_paSpace[prm_index]);
 
-    Elem* pElem = pSpace->_pElemFirst;
+    Elem* pElem = (ElemEx*)(pSpace->_pElemFirst);
     while(true) {
+        if (pElem->_kindbit & _kind_groupA > 0) {
+            _listGroupA.push(((ElemEx*)pElem)->_pActor);
+        }
+        if (pElem->_kindbit & _kind_groupB > 0) {
+            _listGroupB.push(((ElemEx*)pElem)->_pActor);
+        }
 
         pElem = pElem -> _pNext;
         if (pElem == pSpace->_pElemLast) {
             break;
         }
     }
-    _pRegElemFirst = NULL;
 
+    //ツリーが管理してる、親空間のリストと衝突判定
+    executeBumpChk_RoundRobin(&_listGroupA    , &_listTreeGroupB, false);
+    executeBumpChk_RoundRobin(&_listTreeGroupA, &_listGroupB    , false);
 
+    //同一空間内で_listGroupA x _listGroupB
+    executeBumpChk_RoundRobin(&_listGroupA, &_listGroupB, true);
 
 
     int next_level_index = prm_index*8 + 1; //_papSpace[prm_index] 空間の子空間のモートン順序位置0番の配列要素番号
-    for(int i = next_level_index; i < next_level_index+8; i++) {
-        executeBumpChk(i);
+    if ( next_level_index > _num_space) {
+        //リーフ
+        return;
+    } else {
+        //もぐる
+        for(int i = next_level_index; i < next_level_index+8; i++) {
+            if (_paSpace[i]._kindinfobit & (_kind_groupA | _kind_groupB) > 0) {
+                executeBumpChk(i);
+            }
+        }
+
+        return;
     }
 
 }
-
+void LinearOctreeForActor::executeBumpChk_RoundRobin(CollisionList* prm_pListA, CollisionList* prm_pListB, bool addTreeList) {
+    GgafActor* pActor_A;
+    GgafActor* pActor_B;
+    while(true) {
+        pActor_A = prm_pListA->pop();
+        if (pActor_A == NULL) {
+            break; //終了
+        } else {
+            if (addTreeList) {
+                _listTreeGroupA.push(pActor_A);
+            }
+        }
+        while(true) {
+            pActor_B = prm_pListB->pop();
+            if (pActor_B == NULL) {
+                break;
+            } else {
+                if (addTreeList) {
+                    _listTreeGroupB.push(pActor_B);
+                }
+            }
+            pActor_A->executeBumpChk_MeAnd(pActor_B);
+        }
+    }
+}
 
 
 LinearOctreeForActor::~LinearOctreeForActor() {
