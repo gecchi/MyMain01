42a422a855e5f9a7d70a102af2dc14680dbaccd3
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-11-13 16:58:02 +0000

    git-svn-id: file:///home/gecchi/repos@1621 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- cppTest/src/main.cpp
+++ cppTest/src/main.cpp
@@ -3,14 +3,14 @@
 using namespace GgafCore;
 
 int main() {
-    GgafLinearOctree* pTree =  NEW GgafLinearOctree(6);
+    GgafLinearOctree* pTree =  NEW GgafLinearOctree(0);
     pTree->setRootSpace(-8000 ,-8000 ,-8000 ,8000 ,8000 ,8000);
     GgafLinearOctree::Elem* pElem1 = NEW GgafLinearOctree::Elem(NEW GgafObject(), 1);
     GgafLinearOctree::Elem* pElem2 = NEW GgafLinearOctree::Elem(NEW GgafObject(), 2);
     GgafLinearOctree::Elem* pElem3 = NEW GgafLinearOctree::Elem(NEW GgafObject(), 4);
     //pTree->registElem(pElem1, 10 ,10 ,10, 41 ,51 ,61);
     pTree->registElem(pElem2, -7999 ,-7999 ,-7999, -7000 ,-7000 ,-7000);
-    //pTree->registElem(pElem3, 1 ,1 ,1, 1 ,1 ,1);
+    pTree->registElem(pElem3, 5000 ,-7000 ,3000, 5000 ,-7000 ,3000);
     _TRACE_("----------------------------------------------------------------”ª•ª–Ø")
 
     pTree->putTree();
@@ -20,7 +20,8 @@ int main() {
     pTree->putTree();
     _TRACE_("----------------------------------------------------------------”ª•ª–Ø")
 
-    pElem2->extract();
+    pTree->registElem(pElem2, -7999 ,-7999 ,-7999, -7000 ,-7000 ,-7000);
+    pTree->registElem(pElem3, 5000 ,-7000 ,3000, 5000 ,-7000 ,3000);
 
     pTree->putTree();
     _TRACE_("----------------------------------------------------------------”ª•ª–Ø")
@@ -41,5 +42,5 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
         LPTSTR lpCmdLine,
         int nCmdShow)
 {
-main();
-	}
+    return main();
+}
