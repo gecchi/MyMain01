741d5afa5842d0418ce2aade502a14fe1d37a4a0
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-11-16 16:54:40 +0000

    git-svn-id: file:///home/gecchi/repos@1647 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- cppTest/src/main.cpp
+++ cppTest/src/main.cpp
@@ -2,7 +2,101 @@
 #define DEBUG
 using namespace GgafCore;
 
+class MeshSub;
+class SpriteSub;
+class Status;
+
+
+class Parent {
+public:
+    int _p;
+};
+
+
+
+class Status {
+public:
+    int _point;
+    virtual Status* getThis() {
+        return getSub();
+    }
+
+    virtual Status* getSub() = 0;
+
+};
+
+
+
+
+class MMM : public Parent  {
+public:
+    int _X;
+};
+class Mesh : public MMM, public Status {
+public:
+    int _m;
+    virtual Status* getSub() {
+        return this;
+    }
+
+};
+class MeshSub : public Mesh {
+public:
+    int _v;
+
+};
+
+
+
+class Sprite : public Parent, public Status {
+public:
+    int _s;
+    virtual Status* getSub() {
+        return this;
+    }
+};
+class SpriteSub : public Sprite {
+public:
+    int _a;
+
+};
+
+
+
+void putPoint(Parent* p) {
+    Status* s = (Status*)p;
+    _TRACE_("Status="<<(s->getThis()->_point));
+
+}
+
 int main() {
+    SpriteSub* sprite_sub  = new SpriteSub();
+    MeshSub* mesh_sub  = new MeshSub();
+
+    sprite_sub -> _point = 100;
+    mesh_sub -> _point = 500;
+
+    Parent* pA = (Parent*)sprite_sub;
+    Parent* pB = (Parent*)mesh_sub;
+
+//    Status* statusA;
+//    Status* statusB;
+//
+//    statusA = (Status*)sprite_sub;
+//    statusB = (Status*)mesh_sub;
+
+    putPoint(pA);
+    putPoint(pB);
+
+//    delete statusA;
+//    delete statusB;
+}
+
+
+
+
+
+int main_old() {
     GgafLinearOctree* pTree =  NEW GgafLinearOctree(0);
     pTree->setRootSpace(-8000 ,-8000 ,-8000 ,8000 ,8000 ,8000);
     GgafLinearOctreeElem* pElem1 = NEW GgafLinearOctreeElem(NEW GgafObject(), 1);
