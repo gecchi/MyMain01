f328aac0e658f0c39346792148a3827e4814fb34
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-11-19 12:33:29 +0000

    git-svn-id: file:///home/gecchi/repos@1663 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/enemy/Vesta/EnemyVesta.cpp
+++ MyStg2nd/src/gecchi/actor/enemy/Vesta/EnemyVesta.cpp
@@ -18,11 +18,7 @@ void EnemyVesta::initialize() {
     setBumpable(true);
 
     CmRandomNumberGenerator* pRndGen = CmRandomNumberGenerator::getInstance();
-    pRndGen->changeSeed(GameGlobal::_pSceneGame->_li
-
-
-
-                        feframe);
+    pRndGen->changeSeed(GameGlobal::_pSceneGame->_lifeframe);
     DWORD appearances_renge_Z = (GameGlobal::_lim_MyShip_zleft - GameGlobal::_lim_MyShip_zright) * 3;
     DWORD appearances_renge_Y = (GameGlobal::_lim_MyShip_top - GameGlobal::_lim_MyShip_bottom) * 3;
 
@@ -42,8 +38,8 @@ void EnemyVesta::initialize() {
     for (int i = 0, n = 0; i < (_width_X - _depth_Y)  ; i+= _depth_Y, n++) {
         _pStgChecker->setHitAreaBox(n, i - ((_depth_Y/2.0)/1.5)-(_width_X/2 - _depth_Y/2.0), -((_depth_Y/2.0)/1.5), -(_height_Z/2.0),
                                        i + ((_depth_Y/2.0)/1.5)-(_width_X/2 - _depth_Y/2.0),  ((_depth_Y/2.0)/1.5),  (_height_Z/2.0),
-									   false, false, true
-								   );
+                                       false, false, true
+                                   );
     }
     _pStgChecker->setStatus(100, 99999, 99999, 99999);
     useSe("bomb1");
