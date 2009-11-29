d676f150138e4e5e24a07a9222119c4e5aec8bca
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2009-11-29 17:04:07 +0000

    git-svn-id: file:///home/gecchi/repos@1704 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/background/mesh/BackGroundStar001.cpp
+++ MyStg2nd/src/gecchi/actor/background/mesh/BackGroundStar001.cpp
@@ -7,7 +7,7 @@ using namespace MyStg2nd;
 
 BackGroundStar001::BackGroundStar001(const char* prm_name) :
     GgafDx9MeshActor(prm_name,
-                     "hoshiboshi2",
+                     "hosidx",
                      "BackGroundStarEffect",
                      "BackGroundStarTechnique",
                      NULL ) {
