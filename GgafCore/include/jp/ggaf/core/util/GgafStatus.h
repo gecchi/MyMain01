cf9d6cd4a2f06a303248ad259fdd918eeabb025a
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-01-12 16:27:06 +0000

    git-svn-id: file:///home/gecchi/repos@1877 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafCore/include/jp/ggaf/core/util/GgafStatus.h
+++ GgafCore/include/jp/ggaf/core/util/GgafStatus.h
@@ -2,28 +2,61 @@
 #define GGAFSTATUS_H_
 namespace GgafCore {
 
-
+/**
+ * ステースクラス.
+ * @version 1.00
+ * @since 2009/07/22 誕生日
+ * @author Masatoshi Tsuge
+ */
 class GgafStatus {
 
+    union VALUE {
+      char _char_val;
+      int _int_val;
+      double _double_val;
+    };
 
 public:
     int _len;
-    int* _pa_val;
+    VALUE* _paValue;
 
     GgafStatus(int n) {
         _len = n;
-        _pa_val = NEW int[n];
-        for (int i  = 0; i < n; i++) {
-            _pa_val[i] = 0;
-        }
+        _paValue = NEW VALUE[n];
+        ZeroMemory(_paValue, sizeof(VALUE)*_len);
     }
 
-    virtual int get(int n);
 
-    virtual void set(int n, int val);
+    virtual void set(int n, char val) {
+        _paValue[n]._char_val = val;
+    }
+
+    virtual void set(int n, int val) {
+        _paValue[n]._int_val = val;
+    }
+
+    virtual void set(int n, double val) {
+        _paValue[n]._double_val = val;
+    }
+
+    virtual int get(int n) {
+        return _paValue[n]._int_val;
+    }
+
+    virtual int getChar(int n) {
+        return _paValue[n]._char_val;
+    }
+
+    virtual int getInt(int n) {
+        return _paValue[n]._int_val;
+    }
+
+    virtual double getDouble(int n) {
+        return _paValue[n]._double_val;
+    }
 
     virtual ~GgafStatus() {
-        delete[] _pa_val;
+        delete[] _paValue;
     }
 
 };
--- GgafCore/src/jp/ggaf/core/util/GgafStasus.cpp
+++ GgafCore/src/jp/ggaf/core/util/GgafStasus.cpp
@@ -3,11 +3,4 @@ using namespace std;
 
 using namespace GgafCore;
 
-int GgafStatus::get(int n) {
-    return _pa_val[n];
-}
 
-
-void GgafStatus::set(int n, int val) {
-    _pa_val[n] = val;
-}
