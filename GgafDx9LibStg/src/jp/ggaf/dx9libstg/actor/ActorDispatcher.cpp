d6a7726cc59205d405bccdb28ed7157e4974e744
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-01-12 16:26:51 +0000

    git-svn-id: file:///home/gecchi/repos@1876 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/ActorDispatcher.cpp
+++ GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/ActorDispatcher.cpp
@@ -8,3 +8,12 @@ ActorDispatcher::ActorDispatcher(const char* prm_name) : GgafDummyActor(prm_name
     _class_name = "ActorDispatcher";
     setBumpable(false);
 }
+
+
+void ActorDispatcher::enter(GgafActor* prm_pSub) {
+    addSubLast(prm_pSub);
+    if (prm_pSub->_pStatus) {
+        //Ží•Ê‚ðˆø‚«Œp‚®
+        _pStatus->set(STAT_DEFAULT_ACTOR_KIND, prm_pSub->_pStatus->get(STAT_DEFAULT_ACTOR_KIND));
+    }
+}
