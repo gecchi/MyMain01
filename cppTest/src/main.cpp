242de2a34db3c655608fd5fb08472f52811e18f0
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-01-12 14:51:11 +0000

    コメント整理
    git-svn-id: file:///home/gecchi/repos@1872 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- cppTest/src/main.cpp
+++ cppTest/src/main.cpp
@@ -1,16 +1,38 @@
-#include "../../MyStg2nd/src/MyStg2ndHeader.h"
+//#include "../../MyStg2nd/src/MyStg2ndHeader.h"
+#include "../../GgafCore/include/GgafCommonHeader.h"
 using namespace std;
 
 using namespace GgafCore;
-using namespace GgafDx9Core;
-using namespace GgafDx9LibStg;
-using namespace MyStg2nd;
+//using namespace GgafDx9Core;
+//using namespace GgafDx9LibStg;
+//using namespace MyStg2nd;
 
 int main() {
-	//char rankstr[80];
-	unsigned int u = MyStgUtil::easy_hash("D________efaultMeshTechnique");
-	_TRACE_(u);
-}
+    //char rankstr[80];
+
+    char moji[] = { '\0','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                    '1','2','3','4','5','6','7','8','9','0','_',' ', '\0'};
+    int len = strlen(moji);
+    char str[256];
+    for (int q = 0; q < 256; q++) {
+        str[q] = '\0';
+    }
+
+
+
+
+    for (int n = 1; n < len; n++) {
+        str[n] = n;
+        for (int j = 1 ; j < len; j++) {
+            str[i] =  moji[j];
+            unsigned int u = GgafUtil::easy_hash(str);
+            _TRACE_(str<<"="<<u);
+        }
+    }
+
+
+ }
 
 
 
