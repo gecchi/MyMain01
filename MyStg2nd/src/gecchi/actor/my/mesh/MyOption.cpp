357cd73e73df5ca01a9d019369710370078a0e01
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-03-16 18:00:59 +0000

    git-svn-id: file:///home/gecchi/repos@2319 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/my/mesh/MyOption.cpp
+++ MyStg2nd/src/gecchi/actor/my/mesh/MyOption.cpp
@@ -80,18 +80,18 @@ void MyOption::initialize() {
 
 void MyOption::addRadiusPosition(int prm_len) {
     _radiusPosition += prm_len;
-    angle angZY_ROTANG_X_REV = MyStgUtil::getAngle2D(_Z, _Y); //自分の位置
+    angle angZY_ROTANG_X = MyStgUtil::getAngle2D(-_Z, _Y); //自分の位置
     //rzから単位方向ベクトル
     float vx,vy,vz;
     vx = 0;
-    vy = GgafDx9Util::SIN[GgafDx9GeometryMover::simplifyAng(angZY_ROTANG_X_REV)/ANGLE_RATE];
-    vz = -GgafDx9Util::COS[GgafDx9GeometryMover::simplifyAng(angZY_ROTANG_X_REV)/ANGLE_RATE];
+    vy = GgafDx9Util::SIN[GgafDx9GeometryMover::simplifyAng(angZY_ROTANG_X)/ANGLE_RATE];
+    vz = -GgafDx9Util::COS[GgafDx9GeometryMover::simplifyAng(angZY_ROTANG_X)/ANGLE_RATE];
     _Z = _radiusPosition * vz;
     _Y = _radiusPosition * vy;
 
-    _pMover->setRzMvAng(ANGLE180 - angZY_ROTANG_X_REV + ANGLE90);
-    _pMover->setRyMvAng(-ANGLE90);
-    _angveloMove = ((1.0f*_veloMv / _radiusPosition)*(float)ANGLE180)/PI;
+    _pMover->setRzMvAng(angZY_ROTANG_X + ANGLE90);
+//    _pMover->setRyMvAng(-ANGLE90);
+//    _angveloMove = ((1.0f*_veloMv / _radiusPosition)*(float)ANGLE180)/PI;
 }
 
 void MyOption::processBehavior() {
@@ -135,89 +135,89 @@ void MyOption::processBehavior() {
     //ダミーのアクターを連結しようとしたがいろいろ難しい、Quaternion を使わざるを得ない（のではないか；）。
     //TODO:最適化すべし、処理が重たい。いつか汎用化
 
-    static float sinRX, cosRX, sinRY, cosRY, sinRZ, cosRZ;
-
-    //sinRX = GgafDx9Util::SIN[_pMyOptionParent->_pMover->_angFace[AXIS_X] / ANGLE_RATE];
-    //cosRX = GgafDx9Util::COS[_pMyOptionParent->_pMover->_angFace[AXIS_X] / ANGLE_RATE];
-    sinRZ = GgafDx9Util::SIN[_pMyOptionParent->_pMover->_angFace[AXIS_Z] / ANGLE_RATE];
-    cosRZ = GgafDx9Util::COS[_pMyOptionParent->_pMover->_angFace[AXIS_Z] / ANGLE_RATE];
-    sinRY = GgafDx9Util::SIN[_pMyOptionParent->_pMover->_angFace[AXIS_Y] / ANGLE_RATE];
-    cosRY = GgafDx9Util::COS[_pMyOptionParent->_pMover->_angFace[AXIS_Y] / ANGLE_RATE];
-    //自機を中心にWORLD変換のような旋廻
-
-//_TRACE_("MyOption -----");
-//_TRACE_("MyOption ParentFaceRzRy = "<<(_pMyOptionParent->_pMover->_angFace[AXIS_Z])<<" > "<<(_pMyOptionParent->_pMover->_angFace[AXIS_Y]));
-
-    //if (pCAM->_pos_camera == 1 || pCAM->_pos_camera == 2) {
-        //Y軸回転 ＞ Z軸回転
-        _X = cosRY*cosRZ*_Xorg + cosRY*-sinRZ*_Yorg + sinRY*_Zorg;
-        _Y = sinRZ*_Xorg + cosRZ*_Yorg;
-        _Z = -sinRY*cosRZ*_Xorg + -sinRY*-sinRZ*_Yorg + cosRY*_Zorg;
-    //} else {
-    //	//Z軸回転 ＞ Y軸回転
-    //	_X = cosRZ*cosRY*_Xorg + -sinRZ*_Yorg + cosRZ*sinRY*_Zorg;
-    //	_Y = sinRZ*cosRY*_Xorg + cosRZ*_Yorg + sinRZ*sinRY*_Zorg;
-    //	_Z = -sinRY*_Xorg + cosRY*_Zorg;
-    //}
-
-
-
-    //懐中電灯の照射角が広がるような回転（Quaternionで実現）
-    static float vX_axis,vY_axis,vZ_axis; //回転させたい軸ベクトル
-//_TRACE_("_pMover->_angFace[AXIS_Z],_pMover->_angFace[AXIS_Y]="<<(_pMover->_angFace[AXIS_Z])<<","<<(_pMover->_angFace[AXIS_Y]));
-//    angle zz = _pMyOptionParent->_pMover->_angFace[AXIS_Z] + _pMover->_angRzMv;
-//    angle yy = _pMyOptionParent->_pMover->_angFace[AXIS_Y] + _pMover->_angRyMv;
-//    GgafDx9Util::getNormalizeVectorZY(GgafDx9GeometryMover::simplifyAng(zz),
-//                                      GgafDx9GeometryMover::simplifyAng(yy),
-//                                      vX_axis,
-//                                      vY_axis,
-//                                      vZ_axis);
-
-
-    //if (pCAM->_pos_camera == 1 || pCAM->_pos_camera == 2) {
-        vX_axis = cosRY*cosRZ*_pMover->_vX + cosRY*-sinRZ*_pMover->_vY + sinRY*_pMover->_vZ;
-        vY_axis = sinRZ*_pMover->_vX + cosRZ*_pMover->_vY;
-        vZ_axis = -sinRY*cosRZ*_pMover->_vX + -sinRY*-sinRZ*_pMover->_vY + cosRY*_pMover->_vZ;
-    //} else {
-    //	vX_axis = cosRZ*cosRY*_pMover->_vX + -sinRZ*_pMover->_vY + cosRZ*sinRY*_pMover->_vZ;
-    //	vY_axis = sinRZ*cosRY*_pMover->_vX +  cosRZ*_pMover->_vY + sinRZ*sinRY*_pMover->_vZ;
-    //	vZ_axis = -sinRY*_pMover->_vX + cosRY*_pMover->_vZ;
-    //}
-    static float sinHalf, cosHalf;
-    sinHalf = GgafDx9Util::SIN[_angExpanse/ANGLE_RATE/2]; //_angExpanse=回転させたい角度
-    cosHalf = GgafDx9Util::COS[_angExpanse/ANGLE_RATE/2];
-
-    //計算
-    _Q.set( cosHalf, -vX_axis*sinHalf, -vY_axis*sinHalf, -vZ_axis*sinHalf);  //R
-    _Q.mul(0,
-           _pMyOptionParent->_pMover->_vX,
-           _pMyOptionParent->_pMover->_vY,
-           _pMyOptionParent->_pMover->_vZ); //R*P 回転軸が現在の進行方向ベクトルとなる
-    _Q.mul(cosHalf, vX_axis*sinHalf, vY_axis*sinHalf, vZ_axis*sinHalf); //R*P*Q
-    //_Q._x, _Q._y, _Q._z が回転後の座標となる
-    //Z軸回転、Y軸回転角度を計算
-
-//_TRACE_("MyOption Q vX,vY,vZ="<<_Q._x<<","<<_Q._y<<","<<_Q._z);
-
-    GgafDx9Util::getRzRyAng(
-        (float)_Q._x,
-        (float)_Q._y,
-        (float)_Q._z,
-        _RZ,
-        _RY
-     );
-
-//_TRACE_("MyOption Q RzRy="<<_RZ<<" > "<<_RY);
-
-
-    _RZ = GgafDx9GeometryMover::simplifyAng(_RZ);
-    _RY = GgafDx9GeometryMover::simplifyAng(_RY);
-    //GgafDx9Util::anotherRzRy(_RZ,_RY);
-
-//_TRACE_("MyOption SAIGO RzRy="<<_RZ<<" > "<<_RY);
-    _X += _pMyOptionParent->_X;
-    _Y += _pMyOptionParent->_Y;
-    _Z += _pMyOptionParent->_Z;
+//    static float sinRX, cosRX, sinRY, cosRY, sinRZ, cosRZ;
+//
+//    //sinRX = GgafDx9Util::SIN[_pMyOptionParent->_pMover->_angFace[AXIS_X] / ANGLE_RATE];
+//    //cosRX = GgafDx9Util::COS[_pMyOptionParent->_pMover->_angFace[AXIS_X] / ANGLE_RATE];
+//    sinRZ = GgafDx9Util::SIN[_pMyOptionParent->_pMover->_angFace[AXIS_Z] / ANGLE_RATE];
+//    cosRZ = GgafDx9Util::COS[_pMyOptionParent->_pMover->_angFace[AXIS_Z] / ANGLE_RATE];
+//    sinRY = GgafDx9Util::SIN[_pMyOptionParent->_pMover->_angFace[AXIS_Y] / ANGLE_RATE];
+//    cosRY = GgafDx9Util::COS[_pMyOptionParent->_pMover->_angFace[AXIS_Y] / ANGLE_RATE];
+//    //自機を中心にWORLD変換のような旋廻
+//
+////_TRACE_("MyOption -----");
+////_TRACE_("MyOption ParentFaceRzRy = "<<(_pMyOptionParent->_pMover->_angFace[AXIS_Z])<<" > "<<(_pMyOptionParent->_pMover->_angFace[AXIS_Y]));
+//
+//    //if (pCAM->_pos_camera == 1 || pCAM->_pos_camera == 2) {
+//        //Y軸回転 ＞ Z軸回転
+//        _X = cosRY*cosRZ*_Xorg + cosRY*-sinRZ*_Yorg + sinRY*_Zorg;
+//        _Y = sinRZ*_Xorg + cosRZ*_Yorg;
+//        _Z = -sinRY*cosRZ*_Xorg + -sinRY*-sinRZ*_Yorg + cosRY*_Zorg;
+//    //} else {
+//    //	//Z軸回転 ＞ Y軸回転
+//    //	_X = cosRZ*cosRY*_Xorg + -sinRZ*_Yorg + cosRZ*sinRY*_Zorg;
+//    //	_Y = sinRZ*cosRY*_Xorg + cosRZ*_Yorg + sinRZ*sinRY*_Zorg;
+//    //	_Z = -sinRY*_Xorg + cosRY*_Zorg;
+//    //}
+//
+//
+//
+//    //懐中電灯の照射角が広がるような回転（Quaternionで実現）
+//    static float vX_axis,vY_axis,vZ_axis; //回転させたい軸ベクトル
+////_TRACE_("_pMover->_angFace[AXIS_Z],_pMover->_angFace[AXIS_Y]="<<(_pMover->_angFace[AXIS_Z])<<","<<(_pMover->_angFace[AXIS_Y]));
+////    angle zz = _pMyOptionParent->_pMover->_angFace[AXIS_Z] + _pMover->_angRzMv;
+////    angle yy = _pMyOptionParent->_pMover->_angFace[AXIS_Y] + _pMover->_angRyMv;
+////    GgafDx9Util::getNormalizeVectorZY(GgafDx9GeometryMover::simplifyAng(zz),
+////                                      GgafDx9GeometryMover::simplifyAng(yy),
+////                                      vX_axis,
+////                                      vY_axis,
+////                                      vZ_axis);
+//
+//
+//    //if (pCAM->_pos_camera == 1 || pCAM->_pos_camera == 2) {
+//        vX_axis = cosRY*cosRZ*_pMover->_vX + cosRY*-sinRZ*_pMover->_vY + sinRY*_pMover->_vZ;
+//        vY_axis = sinRZ*_pMover->_vX + cosRZ*_pMover->_vY;
+//        vZ_axis = -sinRY*cosRZ*_pMover->_vX + -sinRY*-sinRZ*_pMover->_vY + cosRY*_pMover->_vZ;
+//    //} else {
+//    //	vX_axis = cosRZ*cosRY*_pMover->_vX + -sinRZ*_pMover->_vY + cosRZ*sinRY*_pMover->_vZ;
+//    //	vY_axis = sinRZ*cosRY*_pMover->_vX +  cosRZ*_pMover->_vY + sinRZ*sinRY*_pMover->_vZ;
+//    //	vZ_axis = -sinRY*_pMover->_vX + cosRY*_pMover->_vZ;
+//    //}
+//    static float sinHalf, cosHalf;
+//    sinHalf = GgafDx9Util::SIN[_angExpanse/ANGLE_RATE/2]; //_angExpanse=回転させたい角度
+//    cosHalf = GgafDx9Util::COS[_angExpanse/ANGLE_RATE/2];
+//
+//    //計算
+//    _Q.set( cosHalf, -vX_axis*sinHalf, -vY_axis*sinHalf, -vZ_axis*sinHalf);  //R
+//    _Q.mul(0,
+//           _pMyOptionParent->_pMover->_vX,
+//           _pMyOptionParent->_pMover->_vY,
+//           _pMyOptionParent->_pMover->_vZ); //R*P 回転軸が現在の進行方向ベクトルとなる
+//    _Q.mul(cosHalf, vX_axis*sinHalf, vY_axis*sinHalf, vZ_axis*sinHalf); //R*P*Q
+//    //_Q._x, _Q._y, _Q._z が回転後の座標となる
+//    //Z軸回転、Y軸回転角度を計算
+//
+////_TRACE_("MyOption Q vX,vY,vZ="<<_Q._x<<","<<_Q._y<<","<<_Q._z);
+//
+//    GgafDx9Util::getRzRyAng(
+//        (float)_Q._x,
+//        (float)_Q._y,
+//        (float)_Q._z,
+//        _RZ,
+//        _RY
+//     );
+//
+////_TRACE_("MyOption Q RzRy="<<_RZ<<" > "<<_RY);
+//
+//
+//    _RZ = GgafDx9GeometryMover::simplifyAng(_RZ);
+//    _RY = GgafDx9GeometryMover::simplifyAng(_RY);
+//    //GgafDx9Util::anotherRzRy(_RZ,_RY);
+//
+////_TRACE_("MyOption SAIGO RzRy="<<_RZ<<" > "<<_RY);
+//    _X += _pMyOptionParent->_X;
+//    _Y += _pMyOptionParent->_Y;
+//    _Z += _pMyOptionParent->_Z;
 
     //TODO
     //最適化
