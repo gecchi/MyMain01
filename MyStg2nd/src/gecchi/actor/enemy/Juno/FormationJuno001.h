74fa33dbb0765fc289e846cc6f2ed2a7c74aa194
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-04-01 16:39:26 +0000

    git-svn-id: file:///home/gecchi/repos@2395 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/enemy/Juno/FormationJuno001.h
+++ MyStg2nd/src/gecchi/actor/enemy/Juno/FormationJuno001.h
@@ -5,18 +5,32 @@ namespace MyStg2nd {
 #define NUM_JUNO_FORMATION001 20
 /**
  * フォーメーションアクタークラス .
+ * 自身は出現ポイントの中心
  */
 class FormationJuno001 : public GgafDx9LibStg::FormationActor {
 
     EnemyJuno* _pEnemyJuno[NUM_JUNO_FORMATION001];
 public:
-
+    /** 出現範囲幅BOX */
+    int _X1_app, _Y1_app, _Z1_app, _X2_app, _Y2_app, _Z2_app;
+    /** 全体（出現範囲とJunoの移動方向差分 */
+    angle _incX, _incY, _incZ;
     /** 全体のX */
     static int _X_FormationWhole;
     /** 全体のX増分 */
     static int _incX;
 
+
     FormationJuno001(const char* prm_name);
+
+    void setInitializeProperty(
+            prm_X1_app, prm_Y1_app, prm_Z1_app, prm_X2_app, prm_Y2_app, prm_Z2_app,
+            prm_X, prm_Y, prm_Z,
+            prm_veloMv_App,
+            prm_angRzMv_AppBox, prm_angRyMv_AppBox,
+            prm_veloMv_Juno,
+            prm_angRzMv_JunoMv, prm_angRyMv_Juno,
+            prm_frame_app_interval
 
     virtual void initialize() override;
 
