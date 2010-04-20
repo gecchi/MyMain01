ed98956edcc379fd965036380be09da54e734238
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-04-20 16:19:35 +0000

    git-svn-id: file:///home/gecchi/repos@2465 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/scene/Universe/World/PreDrawScene.cpp
+++ MyStg2nd/src/gecchi/scene/Universe/World/PreDrawScene.cpp
@@ -6,37 +6,7 @@ using namespace GgafDx9LibStg;
 using namespace MyStg2nd;
 
 PreDrawScene::PreDrawScene(const char* prm_name) : DefaultScene(prm_name) {
-    //orderActorToFactory(100000001, HoshiBoshi001, "HoshiBoshi001");
-//    orderActorToFactory(100000002, EnemyTamago01, "gyoku2");
-//    orderActorToFactory(100000003, EnemyTamago01, "gyoku3");
-//    orderActorToFactory(100000004, EnemyTamago01, "gyoku4");
-//    orderActorToFactory(100000005, EnemyTamago01, "gyoku5");
-//    orderActorToFactory(100000006, EnemyTamago01, "gyoku6");
-//    orderActorToFactory(100000007, EnemyTamago01, "gyoku7");
-//    orderActorToFactory(100000008, EnemyTamago01, "gyoku8");
-//    orderActorToFactory(100000009, EnemyTamago01, "gyoku9");
-//    orderActorToFactory(100000010, EnemyTamago01, "gyoku10");
-//    orderActorToFactory(100000011, EnemyTamago01, "gyoku11");
-//    orderActorToFactory(100000012, EnemyTamago01, "gyoku12");
-//    orderActorToFactory(100000013, EnemyTamago01, "gyoku13");
-//    orderActorToFactory(100000014, EnemyTamago01, "gyoku14");
-//    orderActorToFactory(100000015, EnemyTamago01, "gyoku15");
-//    orderActorToFactory(100000016, EnemyTamago01, "gyoku16");
-
-//    _pTurboMeter001 = NEW TurboMeter("TurboMeter001");
-//    _pTurboMeter001->setGeometry(16 , GGAFDX9_PROPERTY(VIEW_SCREEN_HEIGHT) - 64);
-//    getLordActor()->addSubGroup(_pTurboMeter001);
-
-//    orderActorToFactory(100000001, EnemyVesta, "EnemyVesta1");
-//    orderActorToFactory(100000002, EnemyVesta, "EnemyVesta2");
-//    orderActorToFactory(100000003, EnemyVesta, "EnemyVesta3");
-//    orderActorToFactory(100000004, EnemyVesta, "EnemyVesta4");
-//    orderActorToFactory(100000005, EnemyVesta, "EnemyVesta5");
-//    orderActorToFactory(100000006, EnemyVesta, "EnemyVesta6");
-//    orderActorToFactory(100000009, EnemyCirce, "EnemyCirce");
-
-//    orderActorToFactory(100000001, FormationJuno001, "FormationJuno001");
-
+//    orderActorToFactory(10, LaserChipEffectActor, "LaserChipEffectActor");
 }
 
 void PreDrawScene::reset() {
@@ -48,41 +18,8 @@ void PreDrawScene::initialize() {
 }
 
 void PreDrawScene::processBehavior() {
-//    if (getPartFrame() == 120) {
-//        FormationJuno001* p = (FormationJuno001*)obtainActorFromFactory(100000001);
-//        getLordActor()->addSubGroup(p);
-//    }
-//    if (getPartFrame() == 60) {
-//
-//        EnemyVesta* p1 = (EnemyVesta*)obtainActorFromFactory(100000001);
-//        EnemyVesta* p2 = (EnemyVesta*)obtainActorFromFactory(100000002);
-//        EnemyVesta* p3 = (EnemyVesta*)obtainActorFromFactory(100000003);
-//        EnemyVesta* p4 = (EnemyVesta*)obtainActorFromFactory(100000004);
-//        EnemyVesta* p5 = (EnemyVesta*)obtainActorFromFactory(100000005);
-//        EnemyVesta* p6 = (EnemyVesta*)obtainActorFromFactory(100000006);
-//
-//        EnemyCirce* p9 = (EnemyCirce*)obtainActorFromFactory(100000009);
-//        p9->setGeometry(80000,50000,50000);
-//        p9->addSubBone(p1,       0,       0,  300000, ANGLE0,   ANGLE0, ANGLE270);
-//        p9->addSubBone(p2,  300000,       0,       0, ANGLE0,   ANGLE0, ANGLE0);
-//        p9->addSubBone(p3,       0,       0, -300000, ANGLE0,   ANGLE0, ANGLE90);
-//        p9->addSubBone(p4, -300000,       0,       0, ANGLE0,   ANGLE0, ANGLE180);
-//        p9->addSubBone(p5,       0,  300000,       0, ANGLE0,  ANGLE90, ANGLE0);
-//        p9->addSubBone(p6,       0, -300000,       0, ANGLE0, ANGLE270, ANGLE0);
-//
-//        getLordActor()->addSubGroup(p9);
-//
-//    }
-//    for (int i = 1; i <= 1; i++) {
-//        if (getBehaveingFrame() == (i*8)) {
-//            HoshiBoshi001* p = (HoshiBoshi001*)(obtainActorFromFactory(100000000+i));
-//            p->setGeometry(0,0,0);
-//            getLordActor()->addSubGroup(p);
-//        }
-//    }
-
-
-
+//    LaserChipEffectActor* p = (LaserChipEffectActor*)obtainActorFromFactory(10);
+//    getLordActor()->addSubGroup(p);
 }
 
 void PreDrawScene::processFinal() {
