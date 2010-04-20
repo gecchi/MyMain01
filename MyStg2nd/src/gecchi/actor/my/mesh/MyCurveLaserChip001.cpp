6a2ac6a96200c5921d02825eaef7d920765d5f7c
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-04-20 17:33:53 +0000

    git-svn-id: file:///home/gecchi/repos@2468 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/my/mesh/MyCurveLaserChip001.cpp
+++ MyStg2nd/src/gecchi/actor/my/mesh/MyCurveLaserChip001.cpp
@@ -16,7 +16,7 @@ MyCurveLaserChip001::MyCurveLaserChip001(const char* prm_name) :
 
 void MyCurveLaserChip001::initialize() {
     _pMover->relateRzRyFaceAngToMvAng(true);
-    registHitAreaCube(100000);
+    registHitAreaCube(80000);
     setHitAble(true);
     _SX = _SY = _SZ = 100 * 1000;
     _fAlpha = 0.99f;
