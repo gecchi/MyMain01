59f9ecaa13ec8dcabd51746c1394239e4dd8af95
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-04-27 16:22:53 +0000

    git-svn-id: file:///home/gecchi/repos@2502 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/common/shot/Shot004.cpp
+++ MyStg2nd/src/gecchi/actor/common/shot/Shot004.cpp
@@ -21,7 +21,7 @@ void Shot004::initialize() {
 
 void Shot004::onActive() {
     setHitAble(true);
-    _pMover->setMvVelo(20000);             //ˆÚ“®‘¬“x
+    _pMover->setMvVelo(10000);             //ˆÚ“®‘¬“x
 }
 
 void Shot004::processBehavior() {
