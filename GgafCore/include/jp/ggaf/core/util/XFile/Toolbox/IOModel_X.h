c22126b6257a2ecf169ccc32f7cb97a444693cbd
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-06-24 16:35:16 +0000

    git-svn-id: file:///home/gecchi/repos@2717 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

---
+++ GgafCore/include/jp/ggaf/core/util/XFile/Toolbox/IOModel_X.h
@@ -0,0 +1,83 @@
+/////////////////////////////////////////////////////////
+// IOModel_X.h
+// load/save X file descriptions
+//
+/////////////////////////////////////////////////////////
+
+#ifndef IOMODEL_X
+#define IOMODEL_X
+
+//#define DEBUG
+
+
+#include "../Toolbox/MyTrace.h"
+
+#include "../framework/frm_IO.h"
+#include <fstream>
+//#include <GL\freeglut.h>
+#include "../framework/Frm_Mesh.h"
+#include "../ToolBox/XFileStructs.h"
+
+namespace ToolBox {
+
+class IO_Model_X: Frm::IO<Frm::Model3D*> {
+public:
+    IO_Model_X(void) :
+        _LoadSkeletton(0) { //tsuge add
+        _Type = IO_3DX;
+        token_next = 0; //add tsuge
+    }
+    bool Load(std::string pFilename, Frm::Model3D* &pT);
+    bool Save(std::string pFilename, Frm::Model3D* &pT);
+    bool Load(std::string pFilename, std::vector<Frm::Model3D*> &pVT) {
+        return false;
+    }
+    bool Save(std::string pFilename, std::vector<Frm::Model3D*> &pVT) {
+        return false;
+    }
+
+private:
+    ifstream fin;
+    Frm::Bone* _LoadSkeletton;
+    Frm::Mesh* _LoadMesh;
+    Frm::AnimationSet* _LoadAnimationSet;
+    Frm::Model3D* _Object;
+    std::string active_load_filename;
+    //Utilities
+    int16 ProcessBlock(void);
+    int16 BlockID(std::string &pText);
+    void AvoidTemplate(void);
+    void Find(uchar pChar);
+    //Unique X struct ID in case no name are found
+    //Uses the Windows function GetTickCount
+    char* SetUID(char pType);
+    //structure used by the previous function to quickly
+    //convert a 32 bit number to a non-significant text.
+    union {
+        uint32 Integer;
+        //char Text[5];
+        char Text[6]; //mod tsuge
+    } _X_UID;
+
+    /** Xファイル読み込み時の次のトークン予約 */
+    uchar token_next; //add tsuge
+
+    //X File struct processing
+    uint32 _MaxKey; //Max animation key for a loaded animation set
+    void ProcessFrameTransformMatrix(Frm::Bone* &pB);
+    void ProcessBone(Frm::Bone* pBone);
+    void ProcessMesh(void);
+    void ProcessMeshTextureCoords(void);
+    void ProcessMeshMaterials(void);
+    void ProcessMeshNormals(void);
+    void ProcessMaterial(void);
+    void ProcessSkinWeights(void);
+    void ProcessAnimationSets(void);
+    void ProcessAnimations(Frm::AnimationSet* &pAS);
+    void ProcessAnimationKeys(Frm::Animation* &pA);
+    void MapMeshToBones(Frm::Bone* &pBone);
+
+};
+
+}
+#endif
