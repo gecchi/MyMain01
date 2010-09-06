f152d9bfb47a754bb9368f05afe83bc3208e7187
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-09-06 13:34:47 +0000

    ロックオンエフェクトが動く
    git-svn-id: file:///home/gecchi/repos@2978 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/laserchip/CurveLaserChip.cpp
+++ GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/laserchip/CurveLaserChip.cpp
@@ -8,7 +8,6 @@ using namespace GgafDx9LibStg;
 CurveLaserChip::CurveLaserChip(const char* prm_name, const char* prm_model) :
     LaserChip(prm_name, prm_model) {
     _class_name = "CurveLaserChip";
-
     _tmpX = 0;
     _tmpY = 0;
     _tmpZ = 0;
@@ -30,10 +29,9 @@ void CurveLaserChip::onActive() {
     //独自設定したい場合、継承して別クラスを作成し、オーバーライドしてください。
     //その際 は、本クラスの onActive() メソッドも呼び出してください。
     LaserChip::onActive();
-
-//    _tmpX = 0;
-//    _tmpY = 0;
-//    _tmpZ = 0;
+    _tmpX = _X;
+    _tmpY = _Y;
+    _tmpZ = _Z;
 }
 
 void CurveLaserChip::onInactive() {
@@ -44,14 +42,8 @@ void CurveLaserChip::onInactive() {
 }
 
 void CurveLaserChip::processBehavior() {
-
-    //座標に反映
-    //これにより発射元の根元から表示される
-    //if (getActivePartFrame() > 0) {
-        LaserChip::processBehavior();
-        _pMover->behave();
-    //}
-
+    LaserChip::processBehavior();
+    _pMover->behave();
     //座標をコピー
     _tmpX = _X;
     _tmpY = _Y;
@@ -68,10 +60,6 @@ void CurveLaserChip::processSettlementBehavior() {
         //先端
         if (_pChip_behind != NULL && _pChip_behind->isActive()) {
             //普通の先端
-//            CurveLaserChip* pB = (CurveLaserChip*)_pChip_behind;
-//            _X = (_tmpX + pB->_tmpX) / 2;
-//            _Y = (_tmpY + pB->_tmpY) / 2;
-//            _Z = (_tmpZ + pB->_tmpZ) / 2;
         } else {
             //先端で末端
             //どこへでもいきなはれ
@@ -85,49 +73,19 @@ void CurveLaserChip::processSettlementBehavior() {
             //末端で先端
             //どこへでもいきなはれ
         }
-
-
-
     } else if (_pChip_front->isActive() && _pChip_behind->isActive()) {
-//        if (_pChip_front->_pChip_front != NULL && _pChip_front->_pChip_front->isActive()) {
-//            if (_pChip_behind->_pChip_behind != NULL && _pChip_behind->_pChip_behind->isActive()) {
-//                    CurveLaserChip* pFF = (CurveLaserChip*)_pChip_front->_pChip_front;
-//                    CurveLaserChip* pF = (CurveLaserChip*)_pChip_front;
-//                    CurveLaserChip* pB = (CurveLaserChip*)_pChip_behind;
-//                    CurveLaserChip* pBB = (CurveLaserChip*)_pChip_behind->_pChip_behind;
-//                    _X = (pFF->_tmpX + pF->_tmpX + _tmpX + pB->_tmpX + pBB->_tmpX) / 5;
-//                    _Y = (pFF->_tmpY + pF->_tmpY + _tmpY + pB->_tmpY + pBB->_tmpY) / 5;
-//                    _Z = (pFF->_tmpZ + pF->_tmpZ + _tmpZ + pB->_tmpZ + pBB->_tmpZ) / 5;
-//                    _pMover->_veloVxMv = (pFF->_pMover->_veloVxMv + pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv + pBB->_pMover->_veloVxMv) / 5;
-//                    _pMover->_veloVyMv = (pFF->_pMover->_veloVyMv + pF->_pMover->_veloVyMv + _pMover->_veloVyMv + pB->_pMover->_veloVyMv + pBB->_pMover->_veloVyMv) / 5;
-//                    _pMover->_veloVxMv = (pFF->_pMover->_veloVxMv + pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv + pBB->_pMover->_veloVxMv) / 5;
-//			} else {
-//	            CurveLaserChip* pF = (CurveLaserChip*)_pChip_front;
-//	            CurveLaserChip* pB = (CurveLaserChip*)_pChip_behind;
-//	            //中間座標に再設定
-//	            _X = (pF->_tmpX + _tmpX + pB->_tmpX) / 3;
-//	            _Y = (pF->_tmpY + _tmpY + pB->_tmpY) / 3;
-//	            _Z = (pF->_tmpZ + _tmpZ + pB->_tmpZ) / 3;
-//                _pMover->_veloVxMv = (pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv) / 3;
-//                _pMover->_veloVyMv = (pF->_pMover->_veloVyMv + _pMover->_veloVyMv + pB->_pMover->_veloVyMv) / 3;
-//                _pMover->_veloVxMv = (pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv) / 3;
-//
-//			}
-//
-//        } else {
-            //_pChip_behind == NULL の判定だけではだめ。_pChip_behind->isActive()と判定すること
-            //なぜならemployの瞬間に_pChip_behind != NULL となるが、active()により有効になるのは次フレームだから
-            //_X,_Y,_Z にはまだ変な値が入っている。
-            CurveLaserChip* pF = (CurveLaserChip*)_pChip_front;
-            CurveLaserChip* pB = (CurveLaserChip*)_pChip_behind;
-            //中間座標に再設定
-            _X = (pF->_tmpX + _tmpX + pB->_tmpX) / 3;
-            _Y = (pF->_tmpY + _tmpY + pB->_tmpY) / 3;
-            _Z = (pF->_tmpZ + _tmpZ + pB->_tmpZ) / 3;
-            _pMover->_veloVxMv = (pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv) / 3;
-            _pMover->_veloVyMv = (pF->_pMover->_veloVyMv + _pMover->_veloVyMv + pB->_pMover->_veloVyMv) / 3;
-            _pMover->_veloVxMv = (pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv) / 3;
-//        }
+        //_pChip_behind == NULL の判定だけではだめ。_pChip_behind->isActive()と判定すること
+        //なぜならemployの瞬間に_pChip_behind != NULL となるが、active()により有効になるのは次フレームだから
+        //_X,_Y,_Z にはまだ変な値が入っている。
+        CurveLaserChip* pF = (CurveLaserChip*)_pChip_front;
+        CurveLaserChip* pB = (CurveLaserChip*)_pChip_behind;
+        //中間座標に再設定
+        _X = (pF->_tmpX + _tmpX + pB->_tmpX) / 3;
+        _Y = (pF->_tmpY + _tmpY + pB->_tmpY) / 3;
+        _Z = (pF->_tmpZ + _tmpZ + pB->_tmpZ) / 3;
+        _pMover->_veloVxMv = (pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv) / 3;
+        _pMover->_veloVyMv = (pF->_pMover->_veloVyMv + _pMover->_veloVyMv + pB->_pMover->_veloVyMv) / 3;
+        _pMover->_veloVxMv = (pF->_pMover->_veloVxMv + _pMover->_veloVxMv + pB->_pMover->_veloVxMv) / 3;
     }
     LaserChip::processSettlementBehavior();
 }
