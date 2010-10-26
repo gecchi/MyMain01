804d872f2f9868742b4e6a4e37d67e2374669802
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-10-26 15:16:16 +0000

    GgafObjectに _obj_class を持たせる
    git-svn-id: file:///home/gecchi/repos@3171 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/enemy/Iris/EnemyIris.cpp
+++ MyStg2nd/src/gecchi/actor/enemy/Iris/EnemyIris.cpp
@@ -131,7 +131,7 @@ void EnemyIris::onHit(GgafActor* prm_pOtherActor) {
 
         //自機側に撃たれて消滅、かつフォメーション所属の場合、
         //フォーメーションに自身が撃たれた事を伝える。
-        if ((pOther->getKind() & KIND_MY) && (getParent()->_actor_class & Obj_GgafDx9FormationActor)) {
+        if ((pOther->getKind() & KIND_MY) && (getParent()->_obj_class & Obj_GgafDx9FormationActor)) {
             ((GgafDx9FormationActor*)getParent())->wasDestroyedFollower(this);
         }
 
