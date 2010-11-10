9ffb9811878d45fbe35a01e285ab124d018935db
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-11-10 16:18:51 +0000

    git-svn-id: file:///home/gecchi/repos@3221 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/Camera.h
+++ MyStg2nd/src/gecchi/actor/Camera.h
@@ -2,17 +2,6 @@
 #define CAMERA_H_
 
 
-//#define VAM_POS_RIGHT 1
-//#define VAM_POS_LEFT 2
-//#define VAM_POS_TOP 3
-//#define VAM_POS_BOTTOM 4
-//#define VAM_POS_TO_BEHIND 10
-//#define VAM_POS_BEHIND_RIGHT 11
-//#define VAM_POS_BEHIND_LEFT 12
-//#define VAM_POS_BEHIND_TOP 13
-//#define VAM_POS_BEHIND_BOTTOM 14
-
-
 namespace MyStg2nd {
 
 /**
@@ -24,54 +13,6 @@ namespace MyStg2nd {
 class Camera : public GgafDx9LibStg::DefaultCamera {
 
 public:
-//    MyShip* _pMyShip;
-//
-//    /** 原点から初期カメラZ位置の距離 */
-//    int _dZ_camera_init;
-//
-//    /** カメラY座標上限 */
-//    int _lim_CAM_top;
-//    /** カメラY座標下限 */
-//    int _lim_CAM_bottom;
-//    /** カメラX座標上限 */
-//    int _lim_CAM_front;
-//    /** カメラX座標下限 */
-//    int _lim_CAM_behaind;
-//    /** カメラZ座標上限 */
-//    int _lim_CAM_zleft;
-//    /** カメラZ座標下限 */
-//    int _lim_CAM_zright;
-//    /** ビューポイントY座標上限 */
-//    int _lim_VP_top;
-//    /** ビューポイントY座標下限 */
-//    int _lim_VP_bottom;
-//    /** ビューポイントX座標上限 */
-//    int _lim_VP_front;
-//    /** ビューポイントX座標下限 */
-//    int _lim_VP_behaind;
-//    /** ビューポイントZ座標上限 */
-//    int _lim_VP_zleft;
-//    /** ビューポイントZ座標下限 */
-//    int _lim_VP_zright;
-//
-//    /** 現在のカメラのUPのアングル値 */
-//    angle _angXY_nowCamUp;
-//    /** 画面背後時用Z軸補正範囲差分 */
-//    int _correction_width;
-//    /** 画面背後時用Y軸補正範囲差分 */
-//    int _correction_height;
-//    /** カメラ位置番号 */
-//    int _pos_camera;
-//    /** カメラの最高速度（上限、負数で下限） */
-//    int _cam_velo_renge;
-//    /** カメラの速度が、丁度自機の通常移動速度の1.0倍となるカメラ目標座標からの距離。 */
-//    int _stop_renge; //カメラ目標座標距離が、これより小さいと、     1.0倍より減
-//                     //カメラ目標座標距離が、これより大きいとさいと 1.0倍より超
-//    /** カメラ目標座標に加えられるZ軸座標オフセット補正値 */
-//    int _stop_dZ;
-//    /** カメラ目標座標に加えられるY軸座標オフセット補正値 */
-//    int _stop_dY;
-
 
     Camera(const char* prm_name, float prm_rad_fovX, float prm_dep);
 
