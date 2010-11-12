36e03f7fae51b48add56268b21979cb8066b74fa
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-11-12 13:48:31 +0000

    git-svn-id: file:///home/gecchi/repos@3244 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/WorldBoundActor.cpp
+++ GgafDx9LibStg/src/jp/ggaf/dx9libstg/actor/WorldBoundActor.cpp
@@ -12,7 +12,7 @@ WorldBoundActor::WorldBoundActor(const char* prm_name, const char* prm_model) :
     _class_name = "WorldBoundActor";
     _frame_offset = 0;
     _pScaler = NEW GgafDx9GeometricScaler(this);
-    chengeEffectTechnique("NoLight"); //NoLightでベタ塗りテクニック
+    changeEffectTechnique("NoLight"); //NoLightでベタ塗りテクニック
     defineRotMvWorldMatrix(GgafDx9Util::setWorldMatrix_ScMv); //拡大×平行移動のみ
     setSpecialDrawDepth(MAX_DRAW_DEPTH_LEVEL);
 }
