9447dd04431d1bd4d3f9708484994fd98c7babb9
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-12-17 13:42:23 +0000

    Propertiesの保持値を実値にした
    git-svn-id: file:///home/gecchi/repos@3377 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9Core/include/jp/ggaf/dx9core/util/GgafDx9Properties.h
+++ GgafDx9Core/include/jp/ggaf/dx9core/util/GgafDx9Properties.h
@@ -1,28 +1,28 @@
 #ifndef GGAFDX9PROPERTIES_H_
 #define GGAFDX9PROPERTIES_H_
 
-#define GGAFDX9_PROPERTY(X) (*GgafDx9Core::GgafDx9Properties::X)
+#define GGAFDX9_PROPERTY(X) (GgafDx9Core::GgafDx9Properties::X)
 
 namespace GgafDx9Core {
 
 class GgafDx9Properties: public GgafCore::GgafProperties {
 public:
-    static bool* FULL_SCREEN;
-    static int* GAME_BUFFER_WIDTH;
-    static int* GAME_BUFFER_HEIGHT;
-    static float* GAME_SPACE_DEPTH;
-    static int* VIEW_SCREEN_WIDTH;
-    static int* VIEW_SCREEN_HEIGHT;
-    static bool* FIXED_VIEW_ASPECT;
-    static int* MASTER_VOLUME;
-    static int* BGM_VOLUME;
-    static int* SE_VOLUME;
-    static std::string* DIR_MESH_MODEL;
-    static std::string* DIR_SPRITE_MODEL;
-    static std::string* DIR_TEXTURE;
-    static std::string* DIR_EFFECT;
-    static std::string* DIR_OGG;
-    static std::string* DIR_WAVE;
+    static bool FULL_SCREEN;
+    static int GAME_BUFFER_WIDTH;
+    static int GAME_BUFFER_HEIGHT;
+    static float GAME_SPACE_DEPTH;
+    static int VIEW_SCREEN_WIDTH;
+    static int VIEW_SCREEN_HEIGHT;
+    static bool FIXED_VIEW_ASPECT;
+    static int MASTER_VOLUME;
+    static int BGM_VOLUME;
+    static int SE_VOLUME;
+    static std::string DIR_MESH_MODEL;
+    static std::string DIR_SPRITE_MODEL;
+    static std::string DIR_TEXTURE;
+    static std::string DIR_EFFECT;
+    static std::string DIR_OGG;
+    static std::string DIR_WAVE;
 
     static void load(std::string prm_ini_filename);
     static void clean();
--- GgafDx9Core/src/jp/ggaf/dx9core/GgafDx9Properties.cpp
+++ GgafDx9Core/src/jp/ggaf/dx9core/GgafDx9Properties.cpp
@@ -3,81 +3,64 @@ using namespace std;
 using namespace GgafCore;
 using namespace GgafDx9Core;
 
-bool* GgafDx9Properties::FULL_SCREEN = NULL;
-int* GgafDx9Properties::GAME_BUFFER_WIDTH = NULL;
-int* GgafDx9Properties::GAME_BUFFER_HEIGHT = NULL;
-float* GgafDx9Properties::GAME_SPACE_DEPTH = NULL;
-int* GgafDx9Properties::VIEW_SCREEN_WIDTH = NULL;
-int* GgafDx9Properties::VIEW_SCREEN_HEIGHT = NULL;
-bool* GgafDx9Properties::FIXED_VIEW_ASPECT = NULL;
-int* GgafDx9Properties::MASTER_VOLUME;
-int* GgafDx9Properties::BGM_VOLUME;
-int* GgafDx9Properties::SE_VOLUME;
-string* GgafDx9Properties::DIR_MESH_MODEL = NULL;
-string* GgafDx9Properties::DIR_SPRITE_MODEL = NULL;
-string* GgafDx9Properties::DIR_TEXTURE = NULL;
-string* GgafDx9Properties::DIR_EFFECT = NULL;
-string* GgafDx9Properties::DIR_OGG = NULL;
-string* GgafDx9Properties::DIR_WAVE = NULL;
+bool GgafDx9Properties::FULL_SCREEN = 0;
+int GgafDx9Properties::GAME_BUFFER_WIDTH = 0;
+int GgafDx9Properties::GAME_BUFFER_HEIGHT = 0;
+float GgafDx9Properties::GAME_SPACE_DEPTH = 0;
+int GgafDx9Properties::VIEW_SCREEN_WIDTH = 0;
+int GgafDx9Properties::VIEW_SCREEN_HEIGHT = 0;
+bool GgafDx9Properties::FIXED_VIEW_ASPECT = 0;
+int GgafDx9Properties::MASTER_VOLUME;
+int GgafDx9Properties::BGM_VOLUME;
+int GgafDx9Properties::SE_VOLUME;
+string GgafDx9Properties::DIR_MESH_MODEL = "";
+string GgafDx9Properties::DIR_SPRITE_MODEL = "";
+string GgafDx9Properties::DIR_TEXTURE = "";
+string GgafDx9Properties::DIR_EFFECT = "";
+string GgafDx9Properties::DIR_OGG = "";
+string GgafDx9Properties::DIR_WAVE = "";
 
 void GgafDx9Properties::load(string prm_ini_filename) {
     GgafProperties::load(prm_ini_filename);
 
-    FULL_SCREEN = getBool("FULL_SCREEN");
-    GAME_BUFFER_WIDTH = getInt("GAME_BUFFER_WIDTH");
-    GAME_BUFFER_HEIGHT = getInt("GAME_BUFFER_HEIGHT");
-    GAME_SPACE_DEPTH = getFloat("GAME_SPACE_DEPTH");
-    VIEW_SCREEN_WIDTH = getInt("VIEW_SCREEN_WIDTH");
-    VIEW_SCREEN_HEIGHT = getInt("VIEW_SCREEN_HEIGHT");
-    FIXED_VIEW_ASPECT = getBool("FIXED_VIEW_ASPECT");
-    MASTER_VOLUME = getInt("MASTER_VOLUME");
-    BGM_VOLUME = getInt("BGM_VOLUME");
-    SE_VOLUME = getInt("SE_VOLUME");
+    GgafDx9Properties::FULL_SCREEN = getBool("FULL_SCREEN");
+    GgafDx9Properties::GAME_BUFFER_WIDTH = getInt("GAME_BUFFER_WIDTH");
+    GgafDx9Properties::GAME_BUFFER_HEIGHT = getInt("GAME_BUFFER_HEIGHT");
+    GgafDx9Properties::GAME_SPACE_DEPTH = getFloat("GAME_SPACE_DEPTH");
+    GgafDx9Properties::VIEW_SCREEN_WIDTH = getInt("VIEW_SCREEN_WIDTH");
+    GgafDx9Properties::VIEW_SCREEN_HEIGHT = getInt("VIEW_SCREEN_HEIGHT");
+    GgafDx9Properties::FIXED_VIEW_ASPECT = getBool("FIXED_VIEW_ASPECT");
+    GgafDx9Properties::MASTER_VOLUME = getInt("MASTER_VOLUME");
+    GgafDx9Properties::BGM_VOLUME = getInt("BGM_VOLUME");
+    GgafDx9Properties::SE_VOLUME = getInt("SE_VOLUME");
 
-    DIR_MESH_MODEL = getStr("DIR_MESH_MODEL");
-    DIR_SPRITE_MODEL = getStr("DIR_SPRITE_MODEL");
-    DIR_TEXTURE = getStr("DIR_TEXTURE");
-    DIR_EFFECT = getStr("DIR_EFFECT");
-    DIR_OGG = getStr("DIR_OGG");
-    DIR_WAVE = getStr("DIR_WAVE");
+    GgafDx9Properties::DIR_MESH_MODEL = getStr("DIR_MESH_MODEL");
+    GgafDx9Properties::DIR_SPRITE_MODEL = getStr("DIR_SPRITE_MODEL");
+    GgafDx9Properties::DIR_TEXTURE = getStr("DIR_TEXTURE");
+    GgafDx9Properties::DIR_EFFECT = getStr("DIR_EFFECT");
+    GgafDx9Properties::DIR_OGG = getStr("DIR_OGG");
+    GgafDx9Properties::DIR_WAVE = getStr("DIR_WAVE");
 
-    _TRACE_("GGAFDX9_PROPERTY(FULL_SCREEN)="<<GGAFDX9_PROPERTY(FULL_SCREEN));
-    _TRACE_("GGAFDX9_PROPERTY(GAME_BUFFER_WIDTH)="<<GGAFDX9_PROPERTY(GAME_BUFFER_WIDTH));
-    _TRACE_("GGAFDX9_PROPERTY(GAME_SPACE_DEPTH)="<<GGAFDX9_PROPERTY(GAME_SPACE_DEPTH));
-    _TRACE_("GGAFDX9_PROPERTY(GAME_BUFFER_HEIGHT)="<<GGAFDX9_PROPERTY(GAME_BUFFER_HEIGHT));
-    _TRACE_("GGAFDX9_PROPERTY(VIEW_SCREEN_WIDTH)="<<GGAFDX9_PROPERTY(VIEW_SCREEN_WIDTH));
-    _TRACE_("GGAFDX9_PROPERTY(VIEW_SCREEN_HEIGHT)="<<GGAFDX9_PROPERTY(VIEW_SCREEN_HEIGHT));
-    _TRACE_("GGAFDX9_PROPERTY(FIXED_VIEW_ASPECT)="<<GGAFDX9_PROPERTY(FIXED_VIEW_ASPECT));
-    _TRACE_("GGAFDX9_PROPERTY(MASTER_VOLUME)="<<GGAFDX9_PROPERTY(MASTER_VOLUME));
-    _TRACE_("GGAFDX9_PROPERTY(BGM_VOLUME)="<<GGAFDX9_PROPERTY(BGM_VOLUME));
-    _TRACE_("GGAFDX9_PROPERTY(SE_VOLUME)="<<GGAFDX9_PROPERTY(SE_VOLUME));
+    _TRACE_("GgafDx9Properties::FULL_SCREEN="<<GgafDx9Properties::FULL_SCREEN);
+    _TRACE_("GgafDx9Properties::GAME_BUFFER_WIDTH="<<GgafDx9Properties::GAME_BUFFER_WIDTH);
+    _TRACE_("GgafDx9Properties::GAME_SPACE_DEPTH="<<GgafDx9Properties::GAME_SPACE_DEPTH);
+    _TRACE_("GgafDx9Properties::GAME_BUFFER_HEIGHT="<<GgafDx9Properties::GAME_BUFFER_HEIGHT);
+    _TRACE_("GgafDx9Properties::VIEW_SCREEN_WIDTH="<<GgafDx9Properties::VIEW_SCREEN_WIDTH);
+    _TRACE_("GgafDx9Properties::VIEW_SCREEN_HEIGHT="<<GgafDx9Properties::VIEW_SCREEN_HEIGHT);
+    _TRACE_("GgafDx9Properties::FIXED_VIEW_ASPECT="<<GgafDx9Properties::FIXED_VIEW_ASPECT);
+    _TRACE_("GgafDx9Properties::MASTER_VOLUME="<<GgafDx9Properties::MASTER_VOLUME);
+    _TRACE_("GgafDx9Properties::BGM_VOLUME="<<GgafDx9Properties::BGM_VOLUME);
+    _TRACE_("GgafDx9Properties::SE_VOLUME="<<GgafDx9Properties::SE_VOLUME);
 
-    _TRACE_("GGAFDX9_PROPERTY(DIR_MESH_MODEL)="<<GGAFDX9_PROPERTY(DIR_MESH_MODEL));
-    _TRACE_("GGAFDX9_PROPERTY(DIR_SPRITE_MODEL)="<<GGAFDX9_PROPERTY(DIR_SPRITE_MODEL));
-    _TRACE_("GGAFDX9_PROPERTY(DIR_TEXTURE)="<<GGAFDX9_PROPERTY(DIR_TEXTURE));
-    _TRACE_("GGAFDX9_PROPERTY(DIR_EFFECT)="<<GGAFDX9_PROPERTY(DIR_EFFECT));
-    _TRACE_("GGAFDX9_PROPERTY(DIR_OGG)="<<GGAFDX9_PROPERTY(DIR_OGG));
-    _TRACE_("GGAFDX9_PROPERTY(DIR_WAVE)="<<GGAFDX9_PROPERTY(DIR_WAVE));
+    _TRACE_("GgafDx9Properties::DIR_MESH_MODEL="<<GgafDx9Properties::DIR_MESH_MODEL);
+    _TRACE_("GgafDx9Properties::DIR_SPRITE_MODEL="<<GgafDx9Properties::DIR_SPRITE_MODEL);
+    _TRACE_("GgafDx9Properties::DIR_TEXTURE="<<GgafDx9Properties::DIR_TEXTURE);
+    _TRACE_("GgafDx9Properties::DIR_EFFECT="<<GgafDx9Properties::DIR_EFFECT);
+    _TRACE_("GgafDx9Properties::DIR_OGG="<<GgafDx9Properties::DIR_OGG);
+    _TRACE_("GgafDx9Properties::DIR_WAVE="<<GgafDx9Properties::DIR_WAVE);
     //‘±‚«
 }
 
 void GgafDx9Properties::clean() {
     GgafProperties::clean();
-
-    DELETE_IMPOSSIBLE_NULL(DIR_MESH_MODEL);
-    DELETE_IMPOSSIBLE_NULL(DIR_SPRITE_MODEL);
-    DELETE_IMPOSSIBLE_NULL(DIR_TEXTURE);
-    DELETE_IMPOSSIBLE_NULL(DIR_EFFECT);
-    DELETE_IMPOSSIBLE_NULL(DIR_OGG);
-    DELETE_IMPOSSIBLE_NULL(DIR_WAVE);
-    DELETE_IMPOSSIBLE_NULL(FULL_SCREEN);
-    DELETE_IMPOSSIBLE_NULL(GAME_BUFFER_WIDTH);
-    DELETE_IMPOSSIBLE_NULL(GAME_BUFFER_HEIGHT);
-    DELETE_IMPOSSIBLE_NULL(GAME_SPACE_DEPTH);
-    DELETE_IMPOSSIBLE_NULL(VIEW_SCREEN_WIDTH);
-    DELETE_IMPOSSIBLE_NULL(VIEW_SCREEN_HEIGHT);
-    DELETE_IMPOSSIBLE_NULL(FIXED_VIEW_ASPECT);
-    DELETE_IMPOSSIBLE_NULL(MASTER_VOLUME);
-    DELETE_IMPOSSIBLE_NULL(BGM_VOLUME);
-    DELETE_IMPOSSIBLE_NULL(SE_VOLUME);
 }
