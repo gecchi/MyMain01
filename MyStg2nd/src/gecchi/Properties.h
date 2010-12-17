c669a13eca2770d136defd0d5d3850ebcfa5d56e
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-12-17 13:42:15 +0000

    Propertiesの保持値を実値にした
    git-svn-id: file:///home/gecchi/repos@3376 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/Properties.h
+++ MyStg2nd/src/gecchi/Properties.h
@@ -1,43 +1,43 @@
 #ifndef PROPERTIES_H_
 #define PROPERTIES_H_
 
-#define PROPERTY(X) (*MyStg2nd::Properties::X)
+#define PROPERTY(X) (MyStg2nd::Properties::X)
 
 namespace MyStg2nd {
 
 class Properties: public GgafDx9LibStg::StgProperties {
 public:
-    static std::string* MY_KEY_SHOT1;
-    static std::string* MY_KEY_SHOT2;
-    static std::string* MY_KEY_OPTION;
-    static std::string* MY_KEY_VIEW;
-    static std::string* MY_KEY_TURBO;
-    static std::string* MY_KEY_POWERUP;
-    static std::string* MY_KEY_BUTTON7;
-    static std::string* MY_KEY_BUTTON8;
-    static std::string* MY_KEY_PAUSE;
-    static std::string* MY_KEY_UP;
-    static std::string* MY_KEY_DOWN;
-    static std::string* MY_KEY_LEFT;
-    static std::string* MY_KEY_RIGHT;
-    static std::string* MY_KEY_UI_UP;
-    static std::string* MY_KEY_UI_DOWN;
-    static std::string* MY_KEY_UI_LEFT;
-    static std::string* MY_KEY_UI_RIGHT;
-    static std::string* MY_KEY_UI_EXECUTE;
-    static std::string* MY_KEY_UI_CANCEL;
-    static std::string* MY_KEY_UI_DEBUG;
-    static std::string* MY_JOY_SHOT1;
-    static std::string* MY_JOY_SHOT2;
-    static std::string* MY_JOY_OPTION;
-    static std::string* MY_JOY_VIEW;
-    static std::string* MY_JOY_TURBO;
-    static std::string* MY_JOY_POWERUP;
-    static std::string* MY_JOY_BUTTON7;
-    static std::string* MY_JOY_BUTTON8;
-    static std::string* MY_JOY_PAUSE;
-    static std::string* MY_JOY_UI_EXECUTE;
-    static std::string* MY_JOY_UI_CANCEL;
+    static std::string MY_KEY_SHOT1;
+    static std::string MY_KEY_SHOT2;
+    static std::string MY_KEY_OPTION;
+    static std::string MY_KEY_VIEW;
+    static std::string MY_KEY_TURBO;
+    static std::string MY_KEY_POWERUP;
+    static std::string MY_KEY_BUTTON7;
+    static std::string MY_KEY_BUTTON8;
+    static std::string MY_KEY_PAUSE;
+    static std::string MY_KEY_UP;
+    static std::string MY_KEY_DOWN;
+    static std::string MY_KEY_LEFT;
+    static std::string MY_KEY_RIGHT;
+    static std::string MY_KEY_UI_UP;
+    static std::string MY_KEY_UI_DOWN;
+    static std::string MY_KEY_UI_LEFT;
+    static std::string MY_KEY_UI_RIGHT;
+    static std::string MY_KEY_UI_EXECUTE;
+    static std::string MY_KEY_UI_CANCEL;
+    static std::string MY_KEY_UI_DEBUG;
+    static std::string MY_JOY_SHOT1;
+    static std::string MY_JOY_SHOT2;
+    static std::string MY_JOY_OPTION;
+    static std::string MY_JOY_VIEW;
+    static std::string MY_JOY_TURBO;
+    static std::string MY_JOY_POWERUP;
+    static std::string MY_JOY_BUTTON7;
+    static std::string MY_JOY_BUTTON8;
+    static std::string MY_JOY_PAUSE;
+    static std::string MY_JOY_UI_EXECUTE;
+    static std::string MY_JOY_UI_CANCEL;
 
     static void load(std::string prm_ini_filename);
     static void clean();
