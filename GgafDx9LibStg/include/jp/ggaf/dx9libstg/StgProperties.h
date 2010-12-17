ca7ba45b89cf938faf4e9cd2e93ff7c6f191f77d
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-12-17 13:42:33 +0000

    Propertiesの保持値を実値にした
    git-svn-id: file:///home/gecchi/repos@3378 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/include/jp/ggaf/dx9libstg/StgProperties.h
+++ GgafDx9LibStg/include/jp/ggaf/dx9libstg/StgProperties.h
@@ -1,12 +1,12 @@
 #ifndef STGPROPERTIES_H_
 #define STGPROPERTIES_H_
-#define STG_PROPERTY(X) (*GgafDx9LibStg::StgProperties::X)
+#define STG_PROPERTY(X) (GgafDx9LibStg::StgProperties::X)
 namespace GgafDx9LibStg {
 
 class StgProperties: public GgafDx9Core::GgafDx9Properties {
 public:
-    static std::string* DIR_SCENE_DATA;
-    static std::string* DIR_SPLINE_DATA;
+    static std::string DIR_SCENE_DATA;
+    static std::string DIR_SPLINE_DATA;
 
     static void load(std::string prm_ini_filename);
     static void clean();
--- GgafDx9LibStg/src/jp/ggaf/dx9libstg/StgProperties.cpp
+++ GgafDx9LibStg/src/jp/ggaf/dx9libstg/StgProperties.cpp
@@ -4,21 +4,19 @@ using namespace GgafCore;
 using namespace GgafDx9Core;
 using namespace GgafDx9LibStg;
 
-string* StgProperties::DIR_SCENE_DATA = NULL;
-string* StgProperties::DIR_SPLINE_DATA = NULL;
+string StgProperties::DIR_SCENE_DATA = "";
+string StgProperties::DIR_SPLINE_DATA = "";
 
 
 void StgProperties::load(string prm_ini_filename) {
     GgafDx9Properties::load(prm_ini_filename);
-    DIR_SCENE_DATA      = getStr("DIR_SCENE_DATA");
-    DIR_SPLINE_DATA      = getStr("DIR_SPLINE_DATA");
+    StgProperties::DIR_SCENE_DATA   = getStr("DIR_SCENE_DATA");
+    StgProperties::DIR_SPLINE_DATA  = getStr("DIR_SPLINE_DATA");
 
-    _TRACE_("STG_PROPERTY(DIR_SCENE_DATA)="<<STG_PROPERTY(DIR_SCENE_DATA));
-    _TRACE_("STG_PROPERTY(DIR_SCENE_DATA)="<<STG_PROPERTY(DIR_SPLINE_DATA));
+    _TRACE_("StgProperties::DIR_SCENE_DATA)="<<StgProperties::DIR_SCENE_DATA);
+    _TRACE_("StgProperties::DIR_SCENE_DATA)="<<StgProperties::DIR_SPLINE_DATA);
 }
 
 void StgProperties::clean() {
     GgafDx9Properties::clean();
-    DELETE_IMPOSSIBLE_NULL(DIR_SCENE_DATA);
-    DELETE_IMPOSSIBLE_NULL(DIR_SPLINE_DATA);
 }
