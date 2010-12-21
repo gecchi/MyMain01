df89ac69fdf0267c7b09c0281a8872f373dff6a1
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-12-21 11:37:43 +0000

    1フレーム１回View変換行列設定
    git-svn-id: file:///home/gecchi/repos@3384 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/common/mesh/GroundMeshActor.cpp
+++ MyStg2nd/src/gecchi/actor/common/mesh/GroundMeshActor.cpp
@@ -37,8 +37,8 @@ void GroundMeshActor::processDraw() {
     static ID3DXEffect* pID3DXEffect;
     pID3DXEffect = _pMeshEffect->_pID3DXEffect;
     HRESULT hr;
-    hr = pID3DXEffect->SetMatrix(_pMeshEffect->_h_matView, &P_CAM->_vMatrixView );
-    checkDxException(hr, D3D_OK, "GgafDx9MeshActor::processDraw() SetMatrix(g_matView) Ç…é∏îsÇµÇ‹ÇµÇΩÅB");
+//    hr = pID3DXEffect->SetMatrix(_pMeshEffect->_h_matView, &P_CAM->_vMatrixView );
+//    checkDxException(hr, D3D_OK, "GgafDx9MeshActor::processDraw() SetMatrix(g_matView) Ç…é∏îsÇµÇ‹ÇµÇΩÅB");
     GgafDx9Util::setWorldMatrix_RxRzRyMv(this, _matWorld);
     hr = pID3DXEffect->SetMatrix(_pMeshEffect->_h_matWorld, &_matWorld );
     checkDxException(hr, D3D_OK, "GgafDx9MeshActor::processDraw() SetMatrix(g_matWorld) Ç…é∏îsÇµÇ‹ÇµÇΩÅB");
