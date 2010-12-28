70ee86759637b19ed0c4e709fdea55d37511ed32
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-12-28 12:05:14 +0000

    git-svn-id: file:///home/gecchi/repos@3413 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/StgUtil.h
+++ GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/StgUtil.h
@@ -5,7 +5,75 @@ namespace GgafDx9LibStg {
 class StgUtil: public GgafDx9Core::GgafDx9Util {
 public:
 
-    static int a;
+    /**
+     * Rz変化による nWay弾を撃つ .
+     * @param prm_pFrom 発射元
+     * @param prm_pDispatcher_Shot 発射するショットのディスパッチャー
+     * @param prm_pTarget 目標
+     * @param prm_way Way数
+     * @param prm_angClearance Wayの間隔
+     * @param prm_velo 初期速度
+     * @param prm_acce 加速度
+     */
+    static void shotWay001(GgafDx9Core::GgafDx9GeometricActor* prm_pFrom,
+                           GgafCore::GgafActorDispatcher*      prm_pDispatcher_Shot,
+                           GgafDx9Core::GgafDx9GeometricActor* prm_pTarget,
+                           int prm_way, angle prm_angClearance,
+                           velo prm_velo, acce prm_acce);
+    /**
+     * Rz変化による nWay弾を複数セット同時に撃つ .
+     * @param prm_pFrom        発射元
+     * @param prm_pDispatcher_Shot 発射するショットのディスパッチャー
+     * @param prm_pTarget      目標
+     * @param prm_way          Way数
+     * @param prm_angClearance Wayの間隔
+     * @param prm_velo_top     初弾WAYの初期速度
+     * @param prm_acce_top     初弾WAYの加速度
+     * @param prm_num          撃つWAY弾のセット数
+     * @param prm_attenuated   次のWAY弾の初期速度加速度の減衰率
+     */
+    static void shotWay001v2(GgafDx9Core::GgafDx9GeometricActor* prm_pFrom,
+                             GgafCore::GgafActorDispatcher*      prm_pDispatcher_Shot,
+                             GgafDx9Core::GgafDx9GeometricActor* prm_pTarget,
+                             int prm_way, angle prm_angClearance,
+                             velo prm_velo_top, acce prm_acce_top,
+                             int prm_num, float prm_attenuated);
+
+
+    /**
+     * Ry は目標への向き＋ANGLE90で、Rz変化による nWay放射弾を打つ .
+     * @param prm_pFrom 発射元
+     * @param prm_pDispatcher_Shot 発射するショットのディスパッチャー
+     * @param prm_pTarget 目標
+     * @param prm_way 放射Way数
+     * @param prm_angBegin 放射開始アングル
+     * @param prm_velo 初期速度
+     * @param prm_acce 加速度
+     */
+    static void shotWay002(GgafDx9Core::GgafDx9GeometricActor* prm_pFrom,
+                           GgafCore::GgafActorDispatcher*      prm_pDispatcher_Shot,
+                           GgafDx9Core::GgafDx9GeometricActor* prm_pTarget,
+                           int prm_way, angle prm_angBegin,
+                           velo prm_velo, acce prm_acce);
+
+    /**
+     * shotWay002弾を複数セット同時に撃つ .
+     * @param prm_pFrom 発射元
+     * @param prm_pDispatcher_Shot 発射するショットのディスパッチャー
+     * @param prm_pTarget 目標
+     * @param prm_way 放射Way数
+     * @param prm_angBegin 放射開始アングル
+     * @param prm_velo_top 初弾放射WAYの初期速度
+     * @param prm_acce_top 初弾放射WAYの加速度
+     * @param prm_num      撃つ放射WAY弾のセット数
+     * @param prm_attenuated 次のWAY弾の初期速度加速度の減衰率
+     */
+    static void shotWay002v2(GgafDx9Core::GgafDx9GeometricActor* prm_pFrom,
+                             GgafCore::GgafActorDispatcher*      prm_pDispatcher_Shot,
+                             GgafDx9Core::GgafDx9GeometricActor* prm_pTarget,
+                             int prm_way, angle prm_angBegin,
+                             velo prm_velo_top, acce prm_acce_top,
+                             int prm_num, float prm_attenuated);
 
 };
 
--- GgafDx9LibStg/src/jp/ggaf/dx9libstg/util/StgUtil.cpp
+++ GgafDx9LibStg/src/jp/ggaf/dx9libstg/util/StgUtil.cpp
@@ -5,3 +5,120 @@ using namespace GgafCore;
 using namespace GgafDx9Core;
 using namespace GgafDx9LibStg;
 
+
+void StgUtil::shotWay001(GgafDx9GeometricActor* prm_pFrom,
+                       GgafActorDispatcher* prm_pDispatcher_Shot,
+                       GgafDx9GeometricActor* prm_pTarget,
+                       int prm_way, angle prm_angClearance,
+                       velo prm_velo, acce prm_acce) {
+    angle* paAngWay = NEW angle[prm_way];
+    angle rz,ry;
+    GgafDx9Util::getRzRyAng(prm_pTarget->_X - prm_pFrom->_X,
+                            prm_pTarget->_Y - prm_pFrom->_Y,
+                            prm_pTarget->_Z - prm_pFrom->_Z,
+                            rz, ry);
+    GgafDx9Util::getWayAngle2D(0,prm_way, prm_angClearance, paAngWay);
+    GgafDx9DrawableActor* pActor_Shot;
+    for (int i = 0; i < prm_way; i++) {
+        pActor_Shot = (GgafDx9DrawableActor*)prm_pDispatcher_Shot->employ();
+        if (pActor_Shot) {
+            pActor_Shot->setCoordinateBy(prm_pFrom);
+            pActor_Shot->_pMover->setRzRyMvAng(rz+paAngWay[i], ry);
+            pActor_Shot->_pMover->setMvVelo(prm_velo);
+            pActor_Shot->_pMover->setMvAcce(prm_acce);
+            pActor_Shot->activate();
+        }
+    }
+    DELETEARR_IMPOSSIBLE_NULL(paAngWay);
+}
+
+void StgUtil::shotWay001v2(GgafDx9Core::GgafDx9GeometricActor* prm_pFrom,
+                           GgafCore::GgafActorDispatcher*     prm_pDispatcher_Shot,
+                           GgafDx9Core::GgafDx9GeometricActor* prm_pTarget,
+                           int prm_way, angle prm_angClearance,
+                           velo prm_velo_top, acce prm_acce_top,
+                           int prm_num, float prm_attenuated) {
+    angle* paAngWay = NEW angle[prm_way];
+    angle rz,ry;
+    GgafDx9Util::getRzRyAng(prm_pTarget->_X - prm_pFrom->_X,
+                            prm_pTarget->_Y - prm_pFrom->_Y,
+                            prm_pTarget->_Z - prm_pFrom->_Z,
+                            rz, ry);
+    GgafDx9Util::getWayAngle2D(0,prm_way, prm_angClearance, paAngWay);
+    GgafDx9DrawableActor* pActor_Shot;
+    velo now_velo = prm_velo_top;
+    acce now_acce = prm_acce_top;
+    for (int n = 0; n < prm_num; n++) {
+        for (int i = 0; i < prm_way; i++) {
+            pActor_Shot = (GgafDx9DrawableActor*)prm_pDispatcher_Shot->employ();
+            if (pActor_Shot) {
+                pActor_Shot->setCoordinateBy(prm_pFrom);
+                pActor_Shot->_pMover->setRzRyMvAng(rz+paAngWay[i], ry);
+                pActor_Shot->_pMover->setMvVelo(now_velo);
+                pActor_Shot->_pMover->setMvAcce(now_acce);
+                pActor_Shot->activate();
+            }
+        }
+        now_velo *= prm_attenuated;
+        now_acce *= prm_attenuated;
+    }
+    DELETEARR_IMPOSSIBLE_NULL(paAngWay);
+}
+
+void StgUtil::shotWay002(GgafDx9Core::GgafDx9GeometricActor* prm_pFrom,
+                        GgafCore::GgafActorDispatcher*     prm_pDispatcher_Shot,
+                        GgafDx9Core::GgafDx9GeometricActor* prm_pTarget,
+                        int prm_way, angle prm_angBegin,
+                        velo prm_velo, acce prm_acce) {
+    angle* paAngWay = NEW angle[prm_way];
+    angle rz,ry;
+    GgafDx9Util::getRzRyAng(prm_pTarget->_X - prm_pFrom->_X,
+                            prm_pTarget->_Y - prm_pFrom->_Y,
+                            prm_pTarget->_Z - prm_pFrom->_Z,
+                            rz, ry);
+    GgafDx9Util::getRadialAngle2D(prm_angBegin, prm_way, paAngWay);
+    GgafDx9DrawableActor* pActor_Shot;
+    for (int i = 0; i < prm_way; i++) {
+        pActor_Shot = (GgafDx9DrawableActor*)prm_pDispatcher_Shot->employ();
+        if (pActor_Shot) {
+            pActor_Shot->setCoordinateBy(prm_pFrom);
+            pActor_Shot->_pMover->setRzRyMvAng(rz+paAngWay[i], ry+ANGLE90);
+            pActor_Shot->_pMover->setMvVelo(prm_velo);
+            pActor_Shot->_pMover->setMvAcce(prm_acce);
+            pActor_Shot->activate();
+        }
+    }
+    DELETEARR_IMPOSSIBLE_NULL(paAngWay);
+}
+void StgUtil::shotWay002v2(GgafDx9Core::GgafDx9GeometricActor* prm_pFrom,
+                           GgafCore::GgafActorDispatcher*     prm_pDispatcher_Shot,
+                           GgafDx9Core::GgafDx9GeometricActor* prm_pTarget,
+                           int prm_way, angle prm_angBegin,
+                           velo prm_velo_top, acce prm_acce_top,
+                           int prm_num, float prm_attenuated) {
+    angle* paAngWay = NEW angle[prm_way];
+    angle rz,ry;
+    GgafDx9Util::getRzRyAng(prm_pTarget->_X - prm_pFrom->_X,
+                            prm_pTarget->_Y - prm_pFrom->_Y,
+                            prm_pTarget->_Z - prm_pFrom->_Z,
+                            rz, ry);
+    GgafDx9Util::getRadialAngle2D(prm_angBegin, prm_way, paAngWay);
+    GgafDx9DrawableActor* pActor_Shot;
+    velo now_velo = prm_velo_top;
+    acce now_acce = prm_acce_top;
+    for (int n = 0; n < prm_num; n++) {
+        for (int i = 0; i < prm_way; i++) {
+            pActor_Shot = (GgafDx9DrawableActor*)prm_pDispatcher_Shot->employ();
+            if (pActor_Shot) {
+                pActor_Shot->setCoordinateBy(prm_pFrom);
+                pActor_Shot->_pMover->setRzRyMvAng(rz+paAngWay[i], ry+ANGLE90);
+                pActor_Shot->_pMover->setMvVelo(now_velo);
+                pActor_Shot->_pMover->setMvAcce(now_acce);
+                pActor_Shot->activate();
+            }
+        }
+        now_velo *= prm_attenuated;
+        now_acce *= prm_attenuated;
+    }
+    DELETEARR_IMPOSSIBLE_NULL(paAngWay);
+}
