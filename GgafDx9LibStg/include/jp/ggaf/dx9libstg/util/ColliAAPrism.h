84f46c16596c299510f67e53f90baaa501c713b0
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2010-12-28 16:48:07 +0000

    git-svn-id: file:///home/gecchi/repos@3416 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/ColliAAPrism.h
+++ GgafDx9LibStg/include/jp/ggaf/dx9libstg/util/ColliAAPrism.h
@@ -2,21 +2,28 @@
 #define COLLIAAPRISM_H_
 namespace GgafDx9LibStg {
 
+                               //   XYZnp
+#define PRISM_XY_pn   (0xd)    //0b 01101
+#define PRISM_XY_np   (0xe)    //0b 01110
+#define PRISM_XY_pp   (0xd)    //0b 01101
+#define PRISM_XY_nn   (0xf)    //0b 01111
+#define PRISM_YZ_pn   (0x19)   //0b 11001
+#define PRISM_YZ_np   (0x1a)   //0b 11010
+#define PRISM_YZ_pp   (0x19)   //0b 11001
+#define PRISM_YZ_nn   (0x1b)   //0b 11011
+#define PRISM_ZX_pn   (0x15)   //0b 10101
+#define PRISM_ZX_np   (0x16)   //0b 10110
+#define PRISM_ZX_pp   (0x15)   //0b 10101
+#define PRISM_ZX_nn   (0x17)   //0b 10111
 
-#define XY_XpYn   1
-#define XY_XnYp   2
-#define XY_XpYp   3
-#define XY_XnYn   4
 
-#define YZ_YpZn   5
-#define YZ_YnZp   6
-#define YZ_YpZp   7
-#define YZ_YnZn   8
-
-#define ZX_ZpXn   9
-#define ZX_ZnXp   10
-#define ZX_ZpXp   11
-#define ZX_ZnXn   12
+#define PRISM_pn      (0x1)    //0b 00001
+#define PRISM_np      (0x2)    //0b 00010
+#define PRISM_pp      (0x1)    //0b 00001
+#define PRISM_nn      (0x3)    //0b 00011
+#define PRISM_XY      (0xc)    //0b 01100
+#define PRISM_YZ      (0x18)   //0b 11000
+#define PRISM_ZX      (0x14)   //0b 10100
 
 /**
  * AAPrism(軸並行プリズム：Axis-Aligned Prism)当たり判定要素クラス
