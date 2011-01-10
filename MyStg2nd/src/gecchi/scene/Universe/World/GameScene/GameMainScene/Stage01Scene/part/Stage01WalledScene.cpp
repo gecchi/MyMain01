6a03f2181d426740ee5d2cc99555a42661897d9c
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2011-01-10 15:56:59 +0000

    git-svn-id: file:///home/gecchi/repos@3465 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/scene/Universe/World/GameScene/GameMainScene/Stage01Scene/part/Stage01WalledScene.cpp
+++ MyStg2nd/src/gecchi/scene/Universe/World/GameScene/GameMainScene/Stage01Scene/part/Stage01WalledScene.cpp
@@ -45,7 +45,7 @@ Stage01WalledScene::Stage01WalledScene(const char* prm_name) : WalledScene(prm_n
     );
 
     //初期スクロールスピード
-    setScroolSpeed(20000);
+    setScroolSpeed(10000);
 
     // 以下の gen01 start ～ end はExcelマクロにより自動生成されたコードです。
     // コードの変更は「シーンCreater.xls」から行う事とする（整合性確保のため）。
