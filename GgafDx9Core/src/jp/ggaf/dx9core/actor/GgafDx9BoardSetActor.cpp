0c22d23d742e7f2dad5fd55fde706d579c230b34
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2011-01-18 12:47:53 +0000

    git-svn-id: file:///home/gecchi/repos@3496 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9Core/src/jp/ggaf/dx9core/actor/GgafDx9BoardSetActor.cpp
+++ GgafDx9Core/src/jp/ggaf/dx9core/actor/GgafDx9BoardSetActor.cpp
@@ -71,67 +71,6 @@ void GgafDx9BoardSetActor::processDraw() {
     }
     GgafDx9Universe::_pActor_DrawActive = pBoardSetActor; //描画セットの最後アクターをセット
     _pBoardSetModel->draw(this, _draw_set_num);
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-//
-//
-//    _draw_set_num = 1; //同一描画深度に、GgafDx9BoardSetActorの同じモデルかつ同じテクニックが
-//                       //連続しているカウント数
-//    GgafDx9DrawableActor* _pNextDrawActor = _pNext_TheSameDrawDepthLevel;
-//    while (true) {
-//        if (_pNextDrawActor != NULL)  {
-//            if (_pNextDrawActor->_pGgafDx9Model == _pBoardSetModel &&
-//                _pNextDrawActor->_hash_technique == _hash_technique
-//            ) {
-//                _draw_set_num++;
-//                if (_draw_set_num > _pBoardSetModel->_set_num) {
-//                    _draw_set_num = _pBoardSetModel->_set_num;
-//                    break;
-//                }
-//                _pNextDrawActor = _pNextDrawActor->_pNext_TheSameDrawDepthLevel;
-//            } else {
-//                break;
-//            }
-//        } else {
-//            break;
-//        }
-//    }
-//
-//    ID3DXEffect* pID3DXEffect = _pBoardSetEffect->_pID3DXEffect;
-//    HRESULT hr;
-//
-//    GgafDx9DrawableActor *pDrawActor;
-//    GgafDx9RectUV* pRectUV_Active;
-//
-//
-//    pDrawActor = this;
-//    for (int i = 0; i < _draw_set_num; i++) {
-//
-//        hr = pID3DXEffect->SetFloat(_pBoardSetEffect->_ahTransformedX[i], pDrawActor->_x);
-//        checkDxException(hr, D3D_OK, "GgafDx9BoardSetModel::draw SetFloat(_ahTransformedX) に失敗しました。");
-//        hr = pID3DXEffect->SetFloat(_pBoardSetEffect->_ahTransformedY[i], pDrawActor->_y);
-//        checkDxException(hr, D3D_OK, "GgafDx9BoardSetModel::draw SetFloat(_ahTransformedY) に失敗しました。");
-//        hr = pID3DXEffect->SetFloat(_pBoardSetEffect->_ahDepthZ[i], pDrawActor->_z);
-//        checkDxException(hr, D3D_OK, "GgafDx9BoardSetModel::draw SetFloat(_ahDepthZ) に失敗しました。");
-//        hr = pID3DXEffect->SetFloat(_pBoardSetEffect->_ahAlpha[i], pDrawActor->_fAlpha);
-//        checkDxException(hr, D3D_OK, "GgafDx9BoardSetModel::draw SetFloat(_ahAlpha) に失敗しました。");
-//
-//        pRectUV_Active = _pBoardSetModel->_paRectUV + (((GgafDx9BoardSetActor*)(pDrawActor))->_pUvFlipper->_pattno_uvflip_now);
-//        hr = pID3DXEffect->SetFloat(_pBoardSetEffect->_ahOffsetU[i], pRectUV_Active->_aUV[0].tu);
-//        checkDxException(hr, D3D_OK, "GgafDx9BoardModel::draw() SetFloat(_hOffsetU) に失敗しました。");
-//        hr = pID3DXEffect->SetFloat(_pBoardSetEffect->_ahOffsetV[i], pRectUV_Active->_aUV[0].tv);
-//        checkDxException(hr, D3D_OK, "GgafDx9BoardModel::draw() SetFloat(_hOffsetV) に失敗しました。");
-//        pDrawActor = pDrawActor -> _pNext_TheSameDrawDepthLevel;
-//        if (i > 0) {
-//            //アクティブを進める
-//            GgafDx9Universe::_pActor_DrawActive = GgafDx9Universe::_pActor_DrawActive->_pNext_TheSameDrawDepthLevel;
-//        }
-//    }
-//    _pBoardSetModel->draw(this, _draw_set_num);
 }
 
 
--- GgafDx9Core/src/jp/ggaf/dx9core/actor/GgafDx9SpriteSetActor.cpp
+++ GgafDx9Core/src/jp/ggaf/dx9core/actor/GgafDx9SpriteSetActor.cpp
@@ -68,66 +68,6 @@ void GgafDx9SpriteSetActor::processDraw() {
     }
     GgafDx9Universe::_pActor_DrawActive = pSpriteSetActor; //描画セットの最後アクターをセット
     _pSpriteSetModel->draw(this, _draw_set_num);
-
-
-//--------------------------------------------------
-//
-//
-//    _draw_set_num = 1; //同一描画深度に、GgafDx9SpriteSetActorの同じモデルかつ同じテクニックが
-//                       //連続しているカウント数
-//    GgafDx9DrawableActor* _pNextDrawActor = _pNext_TheSameDrawDepthLevel;
-//    while (true) {
-//        if (_pNextDrawActor != NULL)  {
-//            if (_pNextDrawActor->_pGgafDx9Model == _pSpriteSetModel &&
-//                _pNextDrawActor->_hash_technique == _hash_technique
-//            ) {
-//                _draw_set_num++;
-//                if (_draw_set_num > _pSpriteSetModel->_set_num) {
-//                    _draw_set_num = _pSpriteSetModel->_set_num;
-//                    break;
-//                }
-//                _pNextDrawActor = _pNextDrawActor->_pNext_TheSameDrawDepthLevel;
-//            } else {
-//                break;
-//            }
-//        } else {
-//            break;
-//        }
-//    }
-//
-//    ID3DXEffect* pID3DXEffect = _pSpriteSetEffect->_pID3DXEffect;
-//    HRESULT hr;
-////    hr = pID3DXEffect->SetMatrix(_pSpriteSetEffect->_h_matView, &P_CAM->_vMatrixView );
-////    checkDxException(hr, D3D_OK, "GgafDx9MeshActor::GgafDx9MeshEffect SetMatrix(g_matView) に失敗しました。");
-//
-//    GgafDx9DrawableActor *pDrawActor;
-//    GgafDx9RectUV* pRectUV_Active;
-//    pDrawActor = this;
-//    for (int i = 0; i < _draw_set_num; i++) {
-//        //(*_pFunc_calcRotMvWorldMatrix)(pDrawActor, pDrawActor->_matWorld);
-//        hr = pID3DXEffect->SetMatrix(_pSpriteSetEffect->_ah_matWorld[i], &(pDrawActor->_matWorld) );
-//        checkDxException(hr, D3D_OK, "GgafDx9SpriteSetActor::processDraw SetMatrix(_h_matWorld) に失敗しました。");
-//        //今回描画のUV
-//
-//        pRectUV_Active = _pSpriteSetModel->_paRectUV + (((GgafDx9SpriteSetActor*)(pDrawActor))->_pUvFlipper->_pattno_uvflip_now);
-//
-//        hr = pID3DXEffect->SetFloat(_pSpriteSetEffect->_ahOffsetU[i], pRectUV_Active->_aUV[0].tu);
-//        checkDxException(hr, D3D_OK, "GgafDx9SpriteSetActor::processDraw() SetFloat(_hOffsetU) に失敗しました。");
-//
-//        hr = pID3DXEffect->SetFloat(_pSpriteSetEffect->_ahOffsetV[i], pRectUV_Active->_aUV[0].tv);
-//        checkDxException(hr, D3D_OK, "GgafDx9SpriteSetActor::processDraw() SetFloat(_hOffsetV) に失敗しました。");
-//        hr = pID3DXEffect->SetFloat(_pSpriteSetEffect->_ahAlpha[i], pDrawActor->_fAlpha);
-//        checkDxException(hr, D3D_OK, "GgafDx9SpriteSetActor::processDraw SetFloat(_fAlpha) に失敗しました。");
-//        pDrawActor = pDrawActor -> _pNext_TheSameDrawDepthLevel;
-//        if (i > 0) {
-//            //アクティブを進める
-//            GgafDx9Universe::_pActor_DrawActive = GgafDx9Universe::_pActor_DrawActive->_pNext_TheSameDrawDepthLevel;
-//        }
-//    }
-//
-//    _pSpriteSetModel->draw(this, _draw_set_num);
-
-
 }
 
 void GgafDx9SpriteSetActor::setAlpha(float prm_fAlpha) {
