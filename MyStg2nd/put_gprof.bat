7276852a7aaa9227d48a8bc6a574338bf7da1ca3
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2011-02-09 12:43:30 +0000

    git-svn-id: file:///home/gecchi/repos@3578 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/put_gprof.bat
+++ MyStg2nd/put_gprof.bat
@@ -1,6 +1,6 @@
-C:\cygwin\mingw\bin\gprof "Release whit gprof\MyStg2nd.exe" gmon.out -p > gprofp_result.txt
-C:\cygwin\mingw\bin\gprof "Release whit gprof\MyStg2nd.exe" gmon.out -q > gprofq_result.txt
-C:\cygwin\mingw\bin\gprof "Release whit gprof\MyStg2nd.exe" gmon.out -A > gprofA_result.txt
+C:\cygwin\mingw\bin\gprof "GprofCdt\MyStg2nd.exe" gmon.out -p > gprofp_result.txt
+C:\cygwin\mingw\bin\gprof "GprofCdt\MyStg2nd.exe" gmon.out -q > gprofq_result.txt
+C:\cygwin\mingw\bin\gprof "GprofCdt\MyStg2nd.exe" gmon.out -A > gprofA_result.txt
 cygstart gprofp_result.txt
 cygstart gprofq_result.txt
 cygstart gprofA_result.txt
