2de240978b8a6503986c0a1bbb0a6beb4e7fd34d
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2011-03-01 17:18:56 +0000

    git-svn-id: file:///home/gecchi/repos@3636 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- GgafDx9Core/src/jp/ggaf/dx9core/actor/GgafDx9D3DXAniMeshActor.cpp
+++ GgafDx9Core/src/jp/ggaf/dx9core/actor/GgafDx9D3DXAniMeshActor.cpp
@@ -23,90 +23,6 @@ GgafDx9D3DXAniMeshActor::GgafDx9D3DXAniMeshActor(const char* prm_name,
     _pD3DXAniMeshEffect = (GgafDx9D3DXAniMeshEffect*)_pGgafDx9Effect;
     _pFunc_calcRotMvWorldMatrix = GgafDx9Util::setWorldMatrix_RxRzRyMv;
     _pPuppeteer = NEW GgafDx9Puppeteer(this);
-//
-//    LPD3DXANIMATIONSET pAnimationSet0;
-//    hr = _pAc->GetAnimationSet(0, &pAnimationSet0); //ID0番のアニメーションセット取得
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    LPD3DXANIMATIONSET pAnimationSet1;
-//    hr = _pAc->GetAnimationSet(2, &pAnimationSet1); //ID1番のアニメーションセット取得
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    LPD3DXANIMATIONSET pAnimationSet2;
-//    hr = _pAc->GetAnimationSet(2, &pAnimationSet2); //ID2番のアニメーションセット取得
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    LPD3DXANIMATIONSET pAnimationSet4;
-//    hr = _pAc->GetAnimationSet(2, &pAnimationSet4); //ID2番のアニメーションセット取得
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//
-//    hr = _pAc->SetTrackAnimationSet(0, pAnimationSet0);//ID0番のアニメーションセットをトラック0番にセット（デフォルトでこうなるはず）
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    hr = _pAc->SetTrackAnimationSet(1, pAnimationSet0);//ID1番のアニメーションセットをトラック1番にセット（デフォルトでこうなるはず）
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-////    hr = _pAc->SetTrackAnimationSet(2, pAnimationSet2);//ID2番のアニメーションセットをトラック2番にセット（デフォルトでこうなるはず）
-////    checkDxException(hr, D3D_OK, "失敗しました。");
-//
-//    hr = _pAc->ResetTime();//グローバル時間を0にする
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//
-//    hr = _pAc->SetTrackPosition(0,0);//トラック0(=ID0番)のローカル時間を0にする
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    hr = _pAc->SetTrackPosition(1,0);//トラック1(=ID1番)のローカル時間を0にする
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-////    hr = _pAc->SetTrackPosition(2,0);//トラック2(=ID2番)のローカル時間を0にする
-////    checkDxException(hr, D3D_OK, "失敗しました。");
-//
-//    hr = _pAc->SetTrackEnable(0,TRUE );//トラック0(=ID0番)を有効
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    hr = _pAc->SetTrackEnable(1,TRUE );//トラック1(=ID1番)を有効
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-////    hr = _pAc->SetTrackEnable(2,TRUE );//トラック2(=ID2番)を有効
-////    checkDxException(hr, D3D_OK, "失敗しました。");
-//
-//    hr = _pAc->SetTrackWeight(0, 1.00);
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    hr = _pAc->SetTrackWeight(1, 1.00);
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-////    hr = _pAc->SetTrackWeight(2, 1.00);
-////    checkDxException(hr, D3D_OK, "失敗しました。");
-//
-//    hr = _pAc->AdvanceTime(0,NULL);//0秒進める（それを反映させる）。
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//
-//    //ID0番(=トラック0)のアニメーションセットの開始～終了までの時間(秒)
-////    _advance_time_per_frame =  pAnimationSet0->GetPeriod()/60.0;
-////    double time_period0 = pAnimationSet0->GetPeriod();
-////    _TRACE_("time_period0="<<time_period0);
-////    double time_set0 = time_period0 / (60.0 / _pD3DXAniMeshModel->_anim_ticks_per_second); //そのアニメーションにかかる秒数
-//    //ID0番(=トラック0)の SetTrackSpeed が 1.0 固定ならば advanceTimePerFrame は time_period0 / (time_set0 * 60.0)
-////    _advance_time_per_frame0 = time_set0 / time_set0*60.0 ; //トラック0番のアニメーションを60分割時間を求める、つまり１ループ1秒にするための時間
-//    hr = _pAc->SetTrackSpeed(0, 1.0f);  //トラック0(=ID0番)のスピードを設定。
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-//    hr = _pAc->SetTrackSpeed(1, 1.0f);
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-////    hr = _pAc->SetTrackSpeed(2, 1.0f);
-////    checkDxException(hr, D3D_OK, "失敗しました。");
-
-
-
-
-
-
-
-
-
-
-    // _pAc->GetMaxNumAnimationSets()
-
-//    if (SUCCEEDED(pAnimController->GetAnimationSetByName(szAnimSetName, &pAnimSet))) {
-//        if (SUCCEEDED(pAnimSet->QueryInterface(IID_ID3DXKeyframedAnimationSet, (LPVOID*)&pKeyFramedAnimSet))) {
-//            UINT dwNumAnimations = pKeyFramedAnimSet->GetNumAnimations();
-//            for (UINT i = 0; i < dwNumAnimations; i++)
-//                HRESULT hr = pKeyFramedAnimSet->UnregisterAnimation(i);
-//        }
-//        pAnimSet->Release();
-//    }
-
-
-
 }
 
 void GgafDx9D3DXAniMeshActor::setAlpha(float prm_fAlpha) {
@@ -128,32 +44,7 @@ void GgafDx9D3DXAniMeshActor::addAlpha(float prm_fAlpha) {
 }
 
 void GgafDx9D3DXAniMeshActor::processDraw() {
-    ID3DXEffect* pID3DXEffect = _pD3DXAniMeshEffect->_pID3DXEffect;
-    HRESULT hr;
-    //ビュー変換行列
-//    hr = pID3DXEffect->SetMatrix(_pD3DXAniMeshEffect->_h_matView, &P_CAM->_vMatrixView );
-//    checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshActor::processDraw() SetMatrix(g_matView) に失敗しました。");
-    //(*_pFunc_calcRotMvWorldMatrix)(this, _matWorld);
-
-    //hr = pID3DXEffect->SetMatrix(_pD3DXAniMeshEffect->_h_matWorld, &_matWorld );
-    //checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshActor::processDraw() SetMatrix(g_matWorld) に失敗しました。");
-    //SetMatrixはモデルでせざるをえない
-
-    // Zバッファを有効に
-    //GgafDx9God::_pID3DDevice9->SetRenderState(D3DRS_ZENABLE, D3DZB_TRUE);
-    // Zバッファ書き込み可
-    //GgafDx9God::_pID3DDevice9->SetRenderState(D3DRS_ZWRITEENABLE, TRUE);
-
-//    HRESULT hr = _pAc->SetTrackPosition(0,_track0time);//トラック0(=ID0番)のローカル時間を0にする
-//    checkDxException(hr, D3D_OK, "失敗しました。");
-
     _pD3DXAniMeshModel->draw(this);
-
-    // Zバッファを無効に
-    //GgafDx9God::_pID3DDevice9->SetRenderState(D3DRS_ZENABLE, D3DZB_FALSE);
-    // Zバッファ書き込み不可
-    //GgafDx9God::_pID3DDevice9->SetRenderState(D3DRS_ZWRITEENABLE, FALSE );
-
 }
 
 GgafDx9D3DXAniMeshActor::~GgafDx9D3DXAniMeshActor() {
--- GgafDx9Core/src/jp/ggaf/dx9core/model/GgafDx9D3DXAniMeshModel.cpp
+++ GgafDx9Core/src/jp/ggaf/dx9core/model/GgafDx9D3DXAniMeshModel.cpp
@@ -17,64 +17,20 @@ GgafDx9D3DXAniMeshModel::GgafDx9D3DXAniMeshModel(char* prm_model_name) : GgafDx9
 
 HRESULT GgafDx9D3DXAniMeshModel::draw(GgafDx9DrawableActor* prm_pActor_Target, int prm_draw_set_num) {
     TRACE4("GgafDx9D3DXAniMeshModel::draw("<<prm_pActor_Target->getName()<<")");
+    HRESULT hr;
     //対象アクター
     GgafDx9D3DXAniMeshActor* pTargetActor = (GgafDx9D3DXAniMeshActor*)prm_pActor_Target;
     //対象MeshActorのエフェクトラッパ
     GgafDx9D3DXAniMeshEffect* pD3DXAniMeshEffect = (GgafDx9D3DXAniMeshEffect*)prm_pActor_Target->_pGgafDx9Effect;
     //対象エフェクト
     ID3DXEffect* pID3DXEffect = pD3DXAniMeshEffect->_pID3DXEffect;
-
-//
-//
-//    if (GgafDx9ModelManager::_pModelLastDraw != this) {
-//        //頂点バッファとインデックスバッファを設定
-//        GgafDx9God::_pID3DDevice9->SetStreamSource(0, _pIDirect3DVertexBuffer9,  0, _size_vertex_unit);
-//        GgafDx9God::_pID3DDevice9->SetFVF(GgafDx9MeshModel::FVF);
-//        GgafDx9God::_pID3DDevice9->SetIndices(_pIDirect3DIndexBuffer9);
-//
-//        hr = pID3DXEffect->SetFloat(pMeshEffect->_h_tex_blink_power, _fPowerBlink);
-//        checkDxException(hr, D3D_OK, "GgafDx9MeshModel::draw() SetFloat(_h_tex_blink_power) に失敗しました。");
-//        hr = pID3DXEffect->SetFloat(pMeshEffect->_h_tex_blink_threshold, _fBlinkThreshold);
-//        checkDxException(hr, D3D_OK, "GgafDx9MeshModel::draw() SetFloat(_h_tex_blink_threshold) に失敗しました。");
-//    }
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    GgafDx9God::_pID3DDevice9->SetFVF(GgafDx9D3DXAniMeshActor::FVF);
-    HRESULT hr;
-
-    //checkDxException(hr, S_OK, "GgafDx9D3DXAniMeshModel::draw() SetTechnique("<<pTargetActor->_technique<<") に失敗しました。");
-    hr = pID3DXEffect->SetFloat(pD3DXAniMeshEffect->_h_tex_blink_power, _fPowerBlink);
-    checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetFloat(_h_tex_blink_power) に失敗しました。");
-    hr = pID3DXEffect->SetFloat(pD3DXAniMeshEffect->_h_tex_blink_threshold, _fBlinkThreshold);
-    checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetFloat(_h_tex_blink_threshold) に失敗しました。");
-    //_Ang += 0.004f;
-
-
-//    // アニメーションをフレーム分進める
-//    _pAcBase->AdvanceTime(_advance_time_per_frame, NULL );
-
-
-    //ワールド変換行列スタックによるワールド変換行列の計算
-//    D3DXMATRIX WorldMat;
-//    D3DXMatrixIdentity(&WorldMat);
-    //D3DXMatrixRotationYawPitchRoll(&Rot, _Ang, _Ang / 2.37f, 0);
-    //D3DXMatrixMultiply(&WorldMat, &WorldMat, &Rot);
-    //&(pTargetActor->_matWorld)
-
+    if (GgafDx9ModelManager::_pModelLastDraw != this) {
+        GgafDx9God::_pID3DDevice9->SetFVF(GgafDx9D3DXAniMeshActor::FVF);
+        hr = pID3DXEffect->SetFloat(pD3DXAniMeshEffect->_h_tex_blink_power, _fPowerBlink);
+        checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetFloat(_h_tex_blink_power) に失敗しました。");
+        hr = pID3DXEffect->SetFloat(pD3DXAniMeshEffect->_h_tex_blink_threshold, _fBlinkThreshold);
+        checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetFloat(_h_tex_blink_threshold) に失敗しました。");
+    }
     pTargetActor->_pPuppeteer->work(); //アニメーション反映
     list< D3DXFRAME_WORLD* > *pDrawList = _stackWorldMat.GetDrawList(); // 描画リストを取得
     list<D3DXFRAME_WORLD*>::iterator it = pDrawList->begin();
@@ -95,21 +51,6 @@ HRESULT GgafDx9D3DXAniMeshModel::draw(GgafDx9DrawableActor* prm_pActor_Target, i
     checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetValue(g_colMaterialDiffuse) に失敗しました。");
 
     for (int i = 0; it != pDrawList->end(); i++, it++) {
-//        if (GgafDx9ModelManager::_pModelLastDraw != this || _dwNumMaterials != 1) {
-//            if (_papTextureCon[i]) {
-//                //テクスチャのセット
-//                GgafDx9God::_pID3DDevice9->SetTexture(0, _papTextureCon[i]->refer()->_pIDirect3DBaseTexture9);
-//            } else {
-//                _TRACE_("GgafDx9D3DXAniMeshModel::draw("<<prm_pActor_Target->getName()<<") テクスチャがありません。white.pngが設定されるべきです。おかしいです");
-//                //無ければテクスチャ無し
-//                GgafDx9God::_pID3DDevice9->SetTexture(0, NULL);
-//            }
-//            //マテリアルのセット
-//            //GgafDx9God::_pID3DDevice9->SetMaterial(&(pTargetActor->_paD3DMaterial9[i]));
-//            hr = pID3DXEffect->SetValue(pD3DXAniMeshEffect->_h_colMaterialDiffuse, &(pTargetActor->_paD3DMaterial9[i].Diffuse), sizeof(D3DCOLORVALUE) );
-//            checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetValue(g_colMaterialDiffuse) に失敗しました。");
-//        }
-
         //描画
         if ((GgafDx9EffectManager::_pEffect_Active != pD3DXAniMeshEffect || GgafDx9DrawableActor::_hash_technique_last_draw != prm_pActor_Target->_hash_technique) && i == 0) {
             if (GgafDx9EffectManager::_pEffect_Active) {
@@ -147,24 +88,8 @@ HRESULT GgafDx9D3DXAniMeshModel::draw(GgafDx9DrawableActor* prm_pActor_Target, i
 #endif
 
         }
-
-        //GgafDx9God::_pID3DDevice9->SetTransform(D3DTS_WORLD, &(*it)->WorldTransMatrix); // ワールド変換行列を設定
-        //_TRACE_("["<<i<<"]○SetMatrix FrameName="<<((*it)->Name)<<" 描画！");
-
-//        D3DXMatrixMultiply(&WorldMat, &((*it)->WorldTransMatrix), &(pTargetActor->_matWorld));
-
-//        _TRACE_("(*it)->WorldTransMatrix=");
-//        putMat(&((*it)->WorldTransMatrix));
-//        _TRACE_("pTargetActor->_matWorld=");
-//        putMat(&(pTargetActor->_matWorld));
-//        _TRACE_("WorldMat=");
-//        putMat(&WorldMat);
-        //hr = pID3DXEffect->SetMatrix(pD3DXAniMeshEffect->_h_matWorld, &((*it)->WorldTransMatrix));
         hr = pID3DXEffect->SetMatrix(pD3DXAniMeshEffect->_h_matWorld, &((*it)->WorldTransMatrix));
         checkDxException(hr, D3D_OK, "["<<i<<"],GgafDx9D3DXAniMeshActor::processDraw() SetMatrix(g_matWorld) に失敗しました。");
-
-        //hr = pID3DXEffect->SetMatrix(_pD3DXAniMeshEffect->_h_matWorld, &_matWorld );
-            //checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshActor::processDraw() SetMatrix(g_matWorld) に失敗しました。");
         if ((*it)->pMeshContainer == NULL) {
             TRACE4("["<<i<<"]×SetMatrix FrameName="<<((*it)->Name)<<" 飛ばし！");
             continue;
@@ -181,8 +106,6 @@ HRESULT GgafDx9D3DXAniMeshModel::draw(GgafDx9DrawableActor* prm_pActor_Target, i
                     checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetValue(g_colMaterialDiffuse) に失敗しました。");
                 }
                 TRACE4("["<<i<<"]["<<j<<"],SetMaterial");
-                //GgafDx9God::_pID3DDevice9->SetMaterial(&(*it)->pMeshContainer->pMaterials[j].MatD3D);
-
                 hr = pID3DXEffect->CommitChanges();
                 checkDxException(hr, D3D_OK, "["<<i<<"],GgafDx9D3DXAniMeshModel::draw() CommitChanges() に失敗しました。");
                 (*it)->pMeshContainer->MeshData.pMesh->DrawSubset(j);
@@ -191,53 +114,6 @@ HRESULT GgafDx9D3DXAniMeshModel::draw(GgafDx9DrawableActor* prm_pActor_Target, i
             }
         }
     }
-
-//
-//
-//
-//    for (DWORD i = 0; i < _dwNumMaterials; i++) {
-//        if (GgafDx9ModelManager::_pModelLastDraw != this || _dwNumMaterials != 1) {
-//            if (_papTextureCon[i]) {
-//                //テクスチャのセット
-//                GgafDx9God::_pID3DDevice9->SetTexture(0, _papTextureCon[i]->refer()->_pIDirect3DBaseTexture9);
-//            } else {
-//                _TRACE_("GgafDx9D3DXAniMeshModel::draw("<<prm_pActor_Target->getName()<<") テクスチャがありません。white.pngが設定されるべきです。おかしいです");
-//                //無ければテクスチャ無し
-//                GgafDx9God::_pID3DDevice9->SetTexture(0, NULL);
-//            }
-//            //マテリアルのセット
-//            //GgafDx9God::_pID3DDevice9->SetMaterial(&(pTargetActor->_paD3DMaterial9[i]));
-//            hr = pID3DXEffect->SetValue(pD3DXAniMeshEffect->_h_colMaterialDiffuse, &(pTargetActor->_paD3DMaterial9[i].Diffuse), sizeof(D3DCOLORVALUE) );
-//            checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() SetValue(g_colMaterialDiffuse) に失敗しました。");
-//        }
-//
-//        //描画
-//        if (GgafDx9EffectManager::_pEffect_Active != pD3DXAniMeshEffect && i == 0) {
-//            if (GgafDx9EffectManager::_pEffect_Active) {
-//                TRACE4("EndPass("<<GgafDx9EffectManager::_pEffect_Active->_pID3DXEffect<<"): /_pEffect_Active="<<GgafDx9EffectManager::_pEffect_Active->_effect_name<<"("<<GgafDx9EffectManager::_pEffect_Active<<")");
-//                hr = GgafDx9EffectManager::_pEffect_Active->_pID3DXEffect->EndPass();
-//                checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() EndPass() に失敗しました。");
-//                hr = GgafDx9EffectManager::_pEffect_Active->_pID3DXEffect->End();
-//                checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() End() に失敗しました。");
-//            }
-//
-//            TRACE4("SetTechnique("<<pTargetActor->_technique<<"): /actor="<<pTargetActor->getName()<<"/model="<<_model_name<<" effect="<<pD3DXAniMeshEffect->_effect_name);
-//            hr = pID3DXEffect->SetTechnique(pTargetActor->_technique);
-//            checkDxException(hr, S_OK, "GgafDx9D3DXAniMeshModel::draw() SetTechnique("<<pTargetActor->_technique<<") に失敗しました。");
-//            TRACE4("BeginPass("<<pID3DXEffect<<"): /actor="<<pTargetActor->getName()<<"/model="<<_model_name<<" effect="<<pD3DXAniMeshEffect->_effect_name);
-//            UINT numPass;
-//            hr = pID3DXEffect->Begin( &numPass, D3DXFX_DONOTSAVESTATE );
-//            checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() Begin() に失敗しました。");
-//            hr = pID3DXEffect->BeginPass(0);
-//            checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() BeginPass(0) に失敗しました。");
-//        } else {
-//            hr = pID3DXEffect->CommitChanges();
-//            checkDxException(hr, D3D_OK, "GgafDx9D3DXAniMeshModel::draw() CommitChanges() に失敗しました。");
-//        }
-//        TRACE4("DrawSubset: /actor="<<pTargetActor->getName()<<"/model="<<_model_name<<" effect="<<pD3DXAniMeshEffect->_effect_name);
-//        hr = _pID3DXAniMesh->DrawSubset(i);
-//
-//    }
     //前回描画モデル名反映
     GgafDx9ModelManager::_pModelLastDraw = this;
     GgafDx9EffectManager::_pEffect_Active = pD3DXAniMeshEffect;
@@ -245,20 +121,6 @@ HRESULT GgafDx9D3DXAniMeshModel::draw(GgafDx9DrawableActor* prm_pActor_Target, i
     GgafGod::_num_actor_drawing++;
     return D3D_OK;
 }
-
-//シェーダー実装前
-//        if (pTargetActor->_SX == LEN_UNIT &&
-//            pTargetActor->_SY == LEN_UNIT &&
-//            pTargetActor->_SZ == LEN_UNIT)
-//        {
-//            hr = _pID3DXAniMesh->DrawSubset(i); //なんて便利なメソッド！
-//        } else {
-//            //拡大縮小がなされているため、カメラ空間にトランスフォームされた後で頂点法線の正規化するように設定（負荷高）
-//            GgafDx9God::_pID3DDevice9->SetRenderState(D3DRS_NORMALIZENORMALS, TRUE);
-//            hr = _pID3DXAniMesh->DrawSubset(i); //なんて便利なメソッド！
-//            GgafDx9God::_pID3DDevice9->SetRenderState(D3DRS_NORMALIZENORMALS, FALSE);
-//        }
-
 
 void GgafDx9D3DXAniMeshModel::restore() {
     TRACE3("GgafDx9D3DXAniMeshModel::restore() " << _model_name << " start");
