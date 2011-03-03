e9858de5a69cad9a4f64ff9dc86cef734c5213a7
root <root@9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7>
2011-03-03 16:32:19 +0000

    git-svn-id: file:///home/gecchi/repos@3647 9df8dbd1-7660-4e89-b1e7-fa117e7ea0c7

--- MyStg2nd/src/gecchi/actor/item/MagicPointItem001.cpp
+++ MyStg2nd/src/gecchi/actor/item/MagicPointItem001.cpp
@@ -19,9 +19,10 @@ MagicPointItem001::MagicPointItem001(const char* prm_name)
 void MagicPointItem001::initialize() {
     setHitAble(true);
     _pCollisionChecker->makeCollision(1);
-    _pCollisionChecker->setColliAAB_Cube(0, 30000);
+    _pCollisionChecker->setColliAAB_Cube(0, 200000);
     _pSeTransmitter->useSe(1);
     _pSeTransmitter->set(0, "yume_ashi_022");
+    useProgress(3);
 }
 
 void MagicPointItem001::onReset() {
@@ -33,6 +34,17 @@ void MagicPointItem001::onReset() {
     _pKuroko->setMvVelo(2000);
     _pKuroko->setMvAcce(10);
     _pKuroko->forceMvVeloRange(0, 5000);
+    _pKuroko->forceVxMvVeloRange(-20000, 20000);
+    _pKuroko->forceVyMvVeloRange(-20000, 20000);
+    _pKuroko->forceVzMvVeloRange(-20000, 20000);
+
+    _pKuroko->setVxMvVelo(0);
+    _pKuroko->setVyMvVelo(0);
+    _pKuroko->setVzMvVelo(0);
+    _pKuroko->setVxMvAcce(0);
+    _pKuroko->setVyMvAcce(0);
+    _pKuroko->setVzMvAcce(0);
+    _pProgress->change(0);
 }
 void MagicPointItem001::onActive() {
 //    _pKuroko->setFaceAngVelo(AXIS_Y, 5*1000);
@@ -40,10 +52,35 @@ void MagicPointItem001::onActive() {
 }
 
 void MagicPointItem001::processBehavior() {
-    if (getActivePartFrame() < 60 && getActivePartFrame() % 10 == 0) {
-        _pKuroko->orderTagettingMvAngSequence(P_MYSHIP,2000,2000,TURN_CLOSE_TO);
+    if (_pProgress->get() == 0) {
+        if (getActivePartFrame() < 60 && getActivePartFrame() % 10 == 0) {
+            _pKuroko->orderTagettingMvAngSequence(P_MYSHIP,2000,2000,TURN_CLOSE_TO);
+        }
+        _pKuroko->behave();
     }
-    _pKuroko->behave();
+    if (_pProgress->get() == 1) {
+        if (_pProgress->isJustChanged()) {
+            _pKuroko->setMvVelo(0);
+            _pKuroko->setMvAcce(0);
+        }
+        float rate = 8.0 - 0.06*_pProgress->getChangedFrom(); //0.06 * 120 = 8.0
+        rate = rate > 0 ? rate : 0;
+        int fdx = P_MYSHIP->_X - (_X + _pKuroko->_veloVxMv*rate);
+        int fdy = P_MYSHIP->_Y - (_Y + _pKuroko->_veloVyMv*rate);
+        int fdz = P_MYSHIP->_Z - (_Z + _pKuroko->_veloVzMv*rate);
+        _pKuroko->setVxMvAcce(fdx);
+        _pKuroko->setVyMvAcce(fdy);
+        _pKuroko->setVzMvAcce(fdz);
+
+        _SX -= 1;
+        _SY -= 1;
+        _SZ -= 1;
+        if (_SX < 10) {
+            sayonara();
+        }
+        P_MYSHIP_SCENE->_pEnagyBar->_enagy += 1;
+        _pKuroko->behave();
+    }
 }
 
 void MagicPointItem001::processJudgement() {
@@ -61,16 +98,19 @@ void MagicPointItem001::onHit(GgafActor* prm_pOtherActor) {
 
 
     if (pOther->getKind() & KIND_MY_BODY)  {
-        P_MYSHIP_SCENE->_pEnagyBar->_enagy += 10;
-        _pSeTransmitter->playImmediately(0);
-        setHitAble(false);
-        EffectExplosion001* pExplo001 = (EffectExplosion001*)P_COMMON_SCENE->_pDP_EffectExplosion001->employ();
-        if (pExplo001) {
-            pExplo001->locateWith(this);
-            pExplo001->activate();
-        }
-        sayonara();
+        _pProgress->change(1);
     }
+//
+//        P_MYSHIP_SCENE->_pEnagyBar->_enagy += 10;
+//        _pSeTransmitter->playImmediately(0);
+//        setHitAble(false);
+//        EffectExplosion001* pExplo001 = (EffectExplosion001*)P_COMMON_SCENE->_pDP_EffectExplosion001->employ();
+//        if (pExplo001) {
+//            pExplo001->locateWith(this);
+//            pExplo001->activate();
+//        }
+//        sayonara();
+//    }
 
 
 }
