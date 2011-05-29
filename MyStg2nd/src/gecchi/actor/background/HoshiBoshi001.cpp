82afd91db403ed24db4a5ebe83c221d3d125d9f5
gecchi <gecchi@dv9200-xp>
2011-05-30 03:05:42 +0900

    non

--- MyStg2nd/src/gecchi/actor/background/HoshiBoshi001.cpp
+++ MyStg2nd/src/gecchi/actor/background/HoshiBoshi001.cpp
@@ -27,7 +27,7 @@ HoshiBoshi001::HoshiBoshi001(const char* prm_name) :
     setSpecialDrawDepth(MAX_DRAW_DEPTH_LEVEL-3);//Å[•”
     //¯‚ÍDIRECTX‹——£-1.0`1.0i-10px`10px)‚ÉŽû‚Ü‚Á‚Ä‚¢‚é‘O’ñ‚ÅA
     //Œ»‹óŠÔ‚Ì‘å‚«‚³‚ÉŽU‚ç‚Î‚ç‚¹‚é
-    _far_rate = 100.0f;
+    _far_rate = 30.0f;
     _SX = _SY = _SZ =  (P_CAM->_zf*LEN_UNIT)*_far_rate;
 }
 
