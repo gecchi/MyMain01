387d10a8f3282275eebbf7626ce96e7c7ee22c8f
gecchi <gecchi@dv9200-xp>
2011-07-31 01:48:04 +0900

    propaties読み込み直し（今頃！）

--- GgafCore/include/jp/ggaf/core/GgafProperties.h
+++ GgafCore/include/jp/ggaf/core/GgafProperties.h
@@ -16,6 +16,9 @@ namespace GgafCore {
  * @since 2008/07/22
  * @author Masatoshi Tsuge
  */
+
+
+
 class GgafProperties {
 
 public:
@@ -27,10 +30,32 @@ public:
     static void load(std::string prm_properties_filename);
     static void clean();
 
-    static std::_MAP_<std::string, std::string>* _pMapProperties;
 
-    static int read(std::string prm_properties_filename);
-    static void parse(char* p);
+
+//    static int read(std::string prm_properties_filename);
+//    static void parse(char* p);
+
+    typedef std::map<std::string, std::string> PropertyMapT;
+    typedef PropertyMapT::value_type           value_type;
+    typedef PropertyMapT::iterator             iterator;
+
+
+
+    static PropertyMapT* _pMapProperties;
+
+    static void read(std::string filename, PropertyMapT* pMap);
+    static void read(std::istream &is, PropertyMapT* pMap);
+    static void write(const char *filename, PropertyMapT* pMap, const char *header = NULL);
+    static void write(std::ostream &os, PropertyMapT* pMap, const char *header = NULL);
+    static void print(std::ostream &os, PropertyMapT* pMap);
+    static inline char m_hex(int nibble)
+    {
+        static const char *digits = "0123456789ABCDEF";
+        return digits[nibble & 0xf];
+    }
+
+
+
     static bool isExistKey(std::string prm_key);
     static std::string getStr(std::string prm_key);
     static bool getBool(std::string prm_key);
--- GgafCore/src/jp/ggaf/core/GgafProperties.cpp
+++ GgafCore/src/jp/ggaf/core/GgafProperties.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 using namespace GgafCore;
 
-_MAP_<string, string>* GgafProperties::_pMapProperties = NULL;
+GgafProperties::PropertyMapT* GgafProperties::_pMapProperties = NULL;
 
 UINT32 GgafProperties::MAX_SKIP_FRAME = 0;
 int GgafProperties::DRAWNUM_TO_SLOWDOWN1 = 0;
@@ -11,11 +11,11 @@ int GgafProperties::DRAWNUM_TO_SLOWDOWN2 = 0;
 float GgafProperties::FPS_TO_CLEAN_GARBAGE_BOX = 55.0f;
 void GgafProperties::load(string prm_properties_filename) {
     if (_pMapProperties == NULL) {
-        _pMapProperties = NEW _MAP_<string, string>();
-        int ret = read(prm_properties_filename);
-        if (ret != 0) {
-            throwGgafCriticalException("GgafProperties::load() Error! "<<prm_properties_filename<<"のread()に失敗。ステート→"<<ret);
-        }
+        _pMapProperties = NEW PropertyMapT();
+        read(prm_properties_filename, _pMapProperties);
+//        if (ret != 0) {
+//            throwGgafCriticalException("GgafProperties::load() Error! "<<prm_properties_filename<<"のread()に失敗。ステート→"<<ret);
+//        }
     }
 
     GgafProperties::MAX_SKIP_FRAME = getUInt("MAX_SKIP_FRAME");
@@ -27,73 +27,252 @@ void GgafProperties::load(string prm_properties_filename) {
     _TRACE_("GgafProperties::DRAWNUM_TO_SLOWDOWN2="<<GgafProperties::DRAWNUM_TO_SLOWDOWN2);
     _TRACE_("GgafProperties::DRAWNUM_TO_SLOWDOWN2="<<GgafProperties::FPS_TO_CLEAN_GARBAGE_BOX);
 }
-
-int GgafProperties::read(string prm_properties_filename) {
-    const char* pChar_Filename = prm_properties_filename.c_str();
-    FILE* pFile = NULL;
-    int r = 0;
-    try {
-        if (!pChar_Filename) {
-            _TRACE_("!pChar_Filename");
-            throw -1;
-        }
-        //errno_t e = fopen_s(&pFile, pChar_Filename, "r"); //WIN32
-        pFile = fopen(pChar_Filename, "r");
-        if (!pFile) {
-            _TRACE_("fopen(\""<<pChar_Filename<<"\", \"r\"); error:");
-            perror("fopen error:");
-            throw -2;
-        }
-        struct stat status;
-        if (stat(pChar_Filename, &status)) {
-            _TRACE_("stat(pChar_Filename, &status) error:");
-            perror("stat error:");
-            throw -3;
-        }
-        vector<char> vecBuffer(status.st_size + 1);
-        fread(&vecBuffer[0], vecBuffer.size(), 1, pFile);
-        vecBuffer[vecBuffer.size() - 1] = '\0';
-
-        parse(&vecBuffer[0]);
-    } catch (int e) {
-        r = e;
-    }
-
-    if (pFile) {
-        fclose(pFile);
-    }
-    return r;
+void GgafProperties::read(string filename, PropertyMapT* pMap)
+{
+    std::ifstream file(filename.c_str());
+    if (!file)
+        throwGgafCriticalException("unable to open properties file");
+    read(file, pMap);
+    file.close();
 }
-void GgafProperties::parse(char* p) {
-    char* pChar_Token = NULL;
-    string key;
-    string value;
-    for (; *p != '\0'; p++) {
-        if (*p == '#') {
-            while (*p++ != '\n') {
-            } //空読み
-            p--;
-            pChar_Token = NULL;
-        } else if (*p == '\n' || *p == '\r') {
-            *p = '\0';
-        } else if (*p == '=') {
-            *p = '\0';
-            key = pChar_Token;
-            pChar_Token = p + 1;
-            while (*p != '\n' && *p != '\r' && *p != '#') {
-                p++;
-            }
-            *p = '\0';
-            value = pChar_Token;
-            _pMapProperties->insert(_MAP_<string, string>::value_type(key, value));
-            pChar_Token = NULL;
-        } else {
-            if (!pChar_Token) {
-                pChar_Token = p;
-            }
+
+void GgafProperties::read(std::istream &is, PropertyMapT* pMap)
+{
+    if (!is)
+        throwGgafCriticalException("unable to read from stream");
+
+    int ch = 0, next = 0;
+
+    ch = is.get();
+
+    while (!is.eof())
+    {
+        switch (ch)
+        {
+        case '#' :
+        case '!' :
+			do  {
+				ch = is.get();
+			} while (!is.eof() && ch >= 0 && ch != '\n' && ch != '\r');
+            continue;
+        case '\n':
+        case '\r':
+        case ' ' :
+        case '\t': ch = is.get(); continue;
         }
+
+        // Read the key
+        std::ostringstream key, val;
+
+        while (!is.eof() && ch >= 0 && ch != '=' && ch != ':' &&
+               ch != ' ' && ch != '\t' && ch != '\n' && ch != '\r')
+        {
+            key << char(ch);
+            ch = is.get();
+        }
+
+        while (!is.eof() && (ch == ' ' || ch == '\t'))
+            ch = is.get();
+
+        if (!is.eof() && (ch == '=' || ch == ':'))
+            ch = is.get();
+
+        while (!is.eof() && (ch == ' ' || ch == '\t'))
+            ch = is.get();
+
+        // Read the value
+        while (!is.eof() && ch >= 0 && ch != '\n' && ch != '\r')
+        {
+            int next = 0;
+//            if (ch == '\\')
+//            {
+//                ch = is.get();
+//                switch (ch)
+//                {
+//                case '\r':
+//                    ch = is.get();
+//                    if (ch != '\n' && ch != ' ' && ch != '\t')
+//                        continue;
+//                    // fall through
+//                case '\n':
+//                    ch = is.get();
+//                    while (!is.eof() && (ch == ' ' || ch == '\t')) { is >> ch; }
+//                    continue;
+//                case 't': ch = '\t'; next = is.get(); break;
+//                case 'n': ch = '\n'; next = is.get(); break;
+//                case 'r': ch = '\r'; next = is.get(); break;
+//                case 'u':
+//                    {
+//                        ch = is.get();
+//                        while (!is.eof() && ch == 'u') { is >> ch; }
+//                        int d = 0;
+//                    loop:
+//                        for (int i = 0 ; !is.eof() && i < 4 ; i++)
+//                        {
+//                            next = is.get();
+//                            switch (ch)
+//                            {
+//                            case '0': case '1': case '2': case '3': case '4':
+//                            case '5': case '6': case '7': case '8': case '9':           d = (d << 4) +      ch - '0'; break;
+//                            case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': d = (d << 4) + 10 + ch - 'a'; break;
+//                            case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': d = (d << 4) + 10 + ch - 'A'; break;
+//                            default:                                                                                  goto loop;
+//                            }
+//                            ch = is.get();
+//                        }
+//                        ch = d;
+//                        break;
+//                    }
+//                default:
+//                    //next = is.get();
+//                    break;
+//                }
+//            }
+//            else
+                next = is.get();
+
+            val << char(ch);
+            ch = next;
+        }
+        //if (DEBUG)
+            _TRACE_("key:" << key.str() << "|value:" << val.str());
+        (*pMap)[key.str()] = val.str();
     }
 }
+
+void GgafProperties::write(const char *filename, PropertyMapT* pMap, const char *header)
+{
+    std::ofstream file(filename);
+    write(file, pMap, header);
+    file.close();
+}
+
+void GgafProperties::write(std::ostream &os, PropertyMapT* pMap, const char *header)
+{
+    if (header != NULL)
+        os << '#' << header << std::endl;
+
+    os << '#';
+    os << " <date> " << std::endl;
+
+    for (iterator it = pMap->begin(), end = pMap->end(); it != end; ++it)
+    {
+        const std::string &key = (*it).first,
+                          &val = (*it).second;
+        os << key << '=';
+
+        bool empty = false;
+
+        for (size_t i = 0, sz = val.size(); i < sz; i++)
+        {
+            char ch = val[i];
+
+            switch (ch)
+            {
+            case '\\': os << '\\' << '\\'; break;
+            case '\t': os << '\\' << 't';  break;
+            case '\n': os << '\\' << 'n';  break;
+            case '\r': os << '\\' << 'r';  break;
+            default:
+                if (ch < ' ' || ch >= 127 || (empty && ch == ' '))
+                    os << '\\' << 'u'
+                       << m_hex((ch >> 12) & 0x0f) << m_hex((ch >> 8) & 0x0f)
+                       << m_hex((ch >>  4) & 0x0f) << m_hex((ch >> 0) & 0x0f);
+                else
+                    os << ch;
+            }
+
+            empty = false;
+        }
+        os << std::endl;
+    }
+}
+
+void GgafProperties::print(std::ostream &os, PropertyMapT* pMap)
+{
+    iterator it = pMap->begin(), end = pMap->end();
+    for (; it != end; ++it)
+        os << (*it).first << "=" << (*it).second << std::endl;
+}
+//int GgafProperties::read(string prm_properties_filename) {
+//    const char* pChar_Filename = prm_properties_filename.c_str();
+//    FILE* pFile = NULL;
+//    int r = 0;
+//    try {
+//        if (!pChar_Filename) {
+//            _TRACE_("!pChar_Filename");
+//            throw -1;
+//        }
+//        //errno_t e = fopen_s(&pFile, pChar_Filename, "r"); //WIN32
+//        pFile = fopen(pChar_Filename, "r");
+//        if (!pFile) {
+//            _TRACE_("fopen(\""<<pChar_Filename<<"\", \"r\"); error:");
+//            perror("fopen error:");
+//            throw -2;
+//        }
+//        struct stat status;
+//        if (stat(pChar_Filename, &status)) {
+//            _TRACE_("stat(pChar_Filename, &status) error:");
+//            perror("stat error:");
+//            throw -3;
+//        }
+//        _TRACE_("status.st_size="<<status.st_size);
+//
+//        vector<char> vecBuffer(status.st_size + 1);
+//        fread(&vecBuffer[0], vecBuffer.size(), 1, pFile);
+//		for (int z = 1; z < 100; z++) {
+//			vecBuffer[vecBuffer.size() - z] = '*';
+//		}
+//        vecBuffer[vecBuffer.size() - 1] = '\0';
+//        _TRACE_("vecBuffer.size()="<<vecBuffer.size());
+//        parse(&vecBuffer[0]);
+//    } catch (int e) {
+//        r = e;
+//    }
+//
+//    if (pFile) {
+//        fclose(pFile);
+//    }
+//    return r;
+//}
+//void GgafProperties::parse(char* p) {
+//    char* pChar_Token = NULL;
+//    string key;
+//    string value;
+//    bool _in_token = false;
+//    for (; *p != '\0'; p++) {
+//		_TRACE_("key p="<<(p[0]==0?'-':p[0])<<"/pChar_Token="<<(pChar_Token==0?"":pChar_Token)<<"/key="<<key<<"/value="<<value);
+//        if (*p == '#') {
+//            while (*p++ != '\n') {
+//            } //空読み
+//            p--;
+//            pChar_Token = NULL;
+//        } else if (*p == ' ' || *p == '\t') {
+//            *p = '\0';
+//        } else if (*p == '\n' || *p == '\r') {
+//            *p = '\0';
+//        } else if (*p == '=') {
+//            *p = '\0';
+//            key = pChar_Token;
+//            pChar_Token = p + 1;
+//
+//            while (*p != '\n' && *p != '\r' && *p != '#') {
+//                if (*p == ' ' || *p == '\t') {
+//                    *p = '\0';
+//                }
+//                p++;
+//            }
+//            *p = '\0';
+//            value = pChar_Token;
+//            _pMapProperties->insert(_MAP_<string, string>::value_type(key, value));
+//            pChar_Token = NULL;
+//        } else {
+//            if (!pChar_Token) {
+//                pChar_Token = p;
+//            }
+//        }
+//    }
+//}
 
 void GgafProperties::clean() {
     _TRACE_("Properties::clean()");
