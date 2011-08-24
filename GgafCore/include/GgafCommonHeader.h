7d675cf703b487934fcabda7025bdbe38088a464
gecchi <gecchi@dv9200-xp>
2011-08-24 22:37:05 +0900

    pp

--- GgafCore/include/GgafCommonHeader.h
+++ GgafCore/include/GgafCommonHeader.h
@@ -334,7 +334,7 @@ typedef std::map<std::string, std::string> GgafStrMap;
 #define Obj_GgafDirector          (0x8)              //0b 00000000 00000000 00000000 00001000
 #define Obj_GgafGroupHead         (0x10)             //0b 00000000 00000000 00000000 00010000
 #define Obj_GgafDisusedActor      (0x20)             //0b 00000000 00000000 00000000 00100000
-#define Obj_GgafActorDepository        (0x40)             //0b 00000000 00000000 00000000 01000000
+#define Obj_GgafActorDepository   (0x40)             //0b 00000000 00000000 00000000 01000000
 
 namespace GgafCore {
 class GgafUtil;
