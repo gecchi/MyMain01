bde3b896ef1e2e160b6c583b64816c4405a0025c
gecchi <gecchi@dv9200-xp>
2012-08-18 22:01:00 +0900

    test

--- VioletVreath/src/jp/gecchi/VioletVreath/GameGlobal.cpp
+++ VioletVreath/src/jp/gecchi/VioletVreath/GameGlobal.cpp
@@ -9,7 +9,6 @@ double GameGlobal::rank_ = 0.0;
 int GameGlobal::rank_up_level_ = 0;
 UINT32 GameGlobal::score_ = 0;
 
-
 void GameGlobal::init() {
     GameGlobal::rank_ = 0.0;
     GameGlobal::rank_up_level_ = 0;
