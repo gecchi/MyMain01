8cb27d28a64a25618cda8b24fa4c008f30423368
gecchi <gecchi@xps8500-win7>
2013-03-21 00:04:18 +0900

    レベルダウン時表示修正

--- VioletVreath/src/jp/gecchi/VioletVreath/actor/my/MagicMeter/MagicLvCursor003.cpp
+++ VioletVreath/src/jp/gecchi/VioletVreath/actor/my/MagicMeter/MagicLvCursor003.cpp
@@ -63,10 +63,13 @@ void MagicLvCursor003::markOnLevelUpCast(int prm_lv) {
 
 void MagicLvCursor003::markOnLevelDownCast(int prm_lv) {
     _pAFader->setAlpha(1.0);
+    _pAFader->intoTargetAlphaLinerUntil(0, 20);
     moveTo(prm_lv);
-    _pUvFlipper->forcePtnRange(0, 3);
-    _pUvFlipper->setActivePtn(0);
-    _pUvFlipper->setFlipMethod(FLIP_ORDER_NOLOOP, 1);
+    _pUvFlipper->forcePtnRange(8, 11);
+    _pUvFlipper->setActivePtn(8);
+    _pUvFlipper->setFlipMethod(FLIP_ORDER_LOOP, 1);
+    _pScaler->setScale(1000);
+    _pScaler->intoTargetScaleLinerUntil(10000, 20);
 }
 
 void MagicLvCursor003::markOnInvoke(int prm_lv) {
@@ -75,6 +78,7 @@ void MagicLvCursor003::markOnInvoke(int prm_lv) {
     _pUvFlipper->setActivePtn(4);
     _pUvFlipper->setFlipMethod(FLIP_ORDER_LOOP, 1);
 }
+
 void MagicLvCursor003::markOnEffect(int prm_lv) {
     _pAFader->setAlpha(1.0);
     _pAFader->intoTargetAlphaLinerUntil(0, 20);
@@ -85,5 +89,6 @@ void MagicLvCursor003::markOnEffect(int prm_lv) {
     _pScaler->setScale(1000);
     _pScaler->intoTargetScaleLinerUntil(10000, 20);
 }
+
 MagicLvCursor003::~MagicLvCursor003() {
 }
