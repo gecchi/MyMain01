0303e7cf5f953fd93b4e83858358727c34fb0b69
gecchi <gecchi@xps8500-win7>
2013-10-08 20:47:52 +0900

    TEST

--- VioletVreath/src/jp/gecchi/VioletVreath/GameGlobal.cpp
+++ VioletVreath/src/jp/gecchi/VioletVreath/GameGlobal.cpp
@@ -12,7 +12,6 @@ int GameGlobal::rank_up_level_ = 0;
 int GameGlobal::score_ = 0;
 QueryRanking GameGlobal::qryRanking_;
 
-
 int GameGlobal::prime_n_[] = {0,
  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,                                                // 20
  73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,                             // 40
